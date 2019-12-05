c71235428685449b856b8b3ae38afc6b52363af6
luopeihuan <luopeihuan@gzyouai.com>
2019-12-05 18:32:20 +0800

    添加game_server表

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/mapper/GameServerMapper.java
@@ -0,0 +1,19 @@
+/*
+* create by mybatis-plus-generator  https://github.com/xiweile
+*/
+package org.jeecg.modules.game.mapper;
+
+import org.jeecg.modules.game.entity.GameServer;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 游戏服务器表 Mapper 接口
+ * </p>
+ *
+ * @author luopeihuan
+ * @since 2019-12-05
+ */
+public interface GameServerMapper extends BaseMapper<GameServer> {
+
+}
---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/IGameServerService.java
@@ -0,0 +1,19 @@
+/*
+* create by mybatis-plus-generator  https://github.com/xiweile
+*/
+package org.jeecg.modules.game.service;
+
+import org.jeecg.modules.game.entity.GameServer;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 游戏服务器表 服务类
+ * </p>
+ *
+ * @author luopeihuan
+ * @since 2019-12-05
+ */
+public interface IGameServerService extends IService<GameServer> {
+
+}
