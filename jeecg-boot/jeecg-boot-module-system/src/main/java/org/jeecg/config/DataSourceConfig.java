01f69684c81642ca7ca747edac288d3499e3f21c
luopeihuan <luopeihuan@gzyouai.com>
2019-12-05 16:47:42 +0800

    去掉无效的代码

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/config/DataSourceConfig.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/config/DataSourceConfig.java
@@ -21,9 +21,6 @@ import java.util.Map;
 @Configuration
 public class DataSourceConfig {
 
-//    private static final String MAPPER_PACKAGE = "org.jeecg.**.mapper";
-//    private static final String MAPPER_LOCATION = "classpath*:/mappers/**/*Mapper.xml";
-
     /**
      * 动态数据源配置
      *
@@ -45,56 +42,6 @@ public class DataSourceConfig {
     public DataSource dataSourceDefault() {
         return DruidDataSourceBuilder.create().build();
     }
-
-//    /**
-//     * 相当于顶部的：
-//     * {@code @MapperScan("com.baomidou.springboot.mapper*")}
-//     * 这里可以扩展，比如使用配置文件来配置扫描Mapper的路径
-//     */
-//    @Bean
-//    public MapperScannerConfigurer mapperScannerConfigurer() {
-//        MapperScannerConfigurer scannerConfigurer = new MapperScannerConfigurer();
-//        //可以通过环境变量获取你的mapper路径,这样mapper扫描可以通过配置文件配置了
-//        scannerConfigurer.setBasePackage(MAPPER_PACKAGE);
-//        return scannerConfigurer;
-//    }
-//
-//    /**
-//     * 自定义配置{@link MybatisSqlSessionFactoryBean}
-//     * 使用mp-boot-starter 完全可以去掉这些配置，使用yml配置方式, 这里只做示范
-//     */
-//    @Bean("sqlSessionFactory")
-//    @Primary
-//    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-//        MybatisSqlSessionFactoryBean sqlSessionFactory = new MybatisSqlSessionFactoryBean();
-//        sqlSessionFactory.setDataSource(dataSource);
-//
-//        Resource[] resources = PATTERN_RESOLVER.getResources(MAPPER_LOCATION);
-//        sqlSessionFactory.setMapperLocations(resources);
-//
-//        PaginationInterceptor pagination = new PaginationInterceptor();
-//        sqlSessionFactory.setPlugins(new Interceptor[]{
-//                pagination,
-//                new PerformanceInterceptor()
-//        });
-//
-//        GlobalConfig globalConfig = new GlobalConfig();
-//        GlobalConfig.DbConfig dbConfig = new GlobalConfig.DbConfig();
-//        dbConfig.setIdType(IdType.UUID).setTableUnderline(true);
-//        globalConfig.setDbConfig(dbConfig);
-//        globalConfig.setBanner(false);
-//
-//        sqlSessionFactory.setGlobalConfig(globalConfig);
-//        return sqlSessionFactory.getObject();
-//    }
-
-//    @Bean
-//    public SqlSessionFactory sqlSessionFactory(DynamicMultipleDataSource dynamicMultipleDataSource) throws
-//            Exception {
-//        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-//        sqlSessionFactoryBean.setDataSource(dynamicMultipleDataSource);
-//        return sqlSessionFactoryBean.getObject();
-//    }
 }
 
 
