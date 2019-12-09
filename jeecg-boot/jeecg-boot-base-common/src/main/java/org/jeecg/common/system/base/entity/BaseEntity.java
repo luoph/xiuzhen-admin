bf981a505169cd5dd0934bc68288545469f4a188
luopeihuan <luopeihuan@gzyouai.com>
2019-12-09 22:43:11 +0800

    添加BaseEntity

---
+++ jeecg-boot/jeecg-boot-base-common/src/main/java/org/jeecg/common/system/base/entity/BaseEntity.java
@@ -0,0 +1,53 @@
+package org.jeecg.common.system.base.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.jeecg.common.constant.TimeConstant;
+import org.jeecgframework.poi.excel.annotation.Excel;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author dangzhenghui@163.com
+ * @version 1.1
+ * @description Entity基类
+ * @date 2019-4-28
+ */
+@Data
+@Accessors(chain = true)
+public class BaseEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 创建人
+     */
+    @ApiModelProperty(value = "创建人")
+    @Excel(name = "创建人", width = 15)
+    private String createBy;
+    /**
+     * 创建时间
+     */
+    @ApiModelProperty(value = "创建时间")
+    @Excel(name = "创建时间", width = 20, format = TimeConstant.DEFAULT_TIME_FORMAT)
+    @JsonFormat(timezone = TimeConstant.DEFAULT_TIMEZONE, pattern = TimeConstant.DEFAULT_TIME_FORMAT)
+    @DateTimeFormat(pattern = TimeConstant.DEFAULT_TIME_FORMAT)
+    private Date createTime;
+    /**
+     * 更新人
+     */
+    @ApiModelProperty(value = "更新人")
+    @Excel(name = "更新人", width = 15)
+    private String updateBy;
+    /**
+     * 更新时间
+     */
+    @ApiModelProperty(value = "更新时间")
+    @Excel(name = "更新时间", width = 20, format = TimeConstant.DEFAULT_TIME_FORMAT)
+    @JsonFormat(timezone = TimeConstant.DEFAULT_TIMEZONE, pattern = TimeConstant.DEFAULT_TIME_FORMAT)
+    @DateTimeFormat(pattern = TimeConstant.DEFAULT_TIME_FORMAT)
+    private Date updateTime;
+}
--- jeecg-boot/jeecg-boot-base-common/src/main/java/org/jeecg/common/system/base/entity/JeecgEntity.java
+++ jeecg-boot/jeecg-boot-base-common/src/main/java/org/jeecg/common/system/base/entity/JeecgEntity.java
@@ -2,16 +2,10 @@ package org.jeecg.common.system.base.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
-import org.jeecg.common.constant.TimeConstant;
-import org.jeecgframework.poi.excel.annotation.Excel;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.io.Serializable;
-import java.util.Date;
 
 /**
  * @author dangzhenghui@163.com
@@ -21,7 +15,8 @@ import java.util.Date;
  */
 @Data
 @Accessors(chain = true)
-public class JeecgEntity implements Serializable {
+@EqualsAndHashCode(callSuper = true)
+public class JeecgEntity extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
     /**
@@ -30,32 +25,4 @@ public class JeecgEntity implements Serializable {
     @TableId(type = IdType.ID_WORKER_STR)
     @ApiModelProperty(value = "ID")
     private String id;
-    /**
-     * 创建人
-     */
-    @ApiModelProperty(value = "创建人")
-    @Excel(name = "创建人", width = 15)
-    private String createBy;
-    /**
-     * 创建时间
-     */
-    @ApiModelProperty(value = "创建时间")
-    @Excel(name = "创建时间", width = 20, format = TimeConstant.DEFAULT_TIME_FORMAT)
-    @JsonFormat(timezone = TimeConstant.DEFAULT_TIMEZONE, pattern = TimeConstant.DEFAULT_TIME_FORMAT)
-    @DateTimeFormat(pattern = TimeConstant.DEFAULT_TIME_FORMAT)
-    private Date createTime;
-    /**
-     * 更新人
-     */
-    @ApiModelProperty(value = "更新人")
-    @Excel(name = "更新人", width = 15)
-    private String updateBy;
-    /**
-     * 更新时间
-     */
-    @ApiModelProperty(value = "更新时间")
-    @Excel(name = "更新时间", width = 20, format = TimeConstant.DEFAULT_TIME_FORMAT)
-    @JsonFormat(timezone = TimeConstant.DEFAULT_TIMEZONE, pattern = TimeConstant.DEFAULT_TIME_FORMAT)
-    @DateTimeFormat(pattern = TimeConstant.DEFAULT_TIME_FORMAT)
-    private Date updateTime;
 }
