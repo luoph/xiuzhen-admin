614cff0eaeb37f32046fcf70ee7637894dd663fc
luopeihuan <luopeihuan@gzyouai.com>
2019-12-09 17:34:50 +0800

    简化代码

--- ant-design-vue-jeecg/src/views/jeecg/JeecgDemoList.vue
+++ ant-design-vue-jeecg/src/views/jeecg/JeecgDemoList.vue
@@ -373,38 +373,5 @@ export default {
 };
 </script>
 <style scoped>
-.ant-card-body .table-operator {
-    margin-bottom: 18px;
-}
-
-.ant-table-tbody .ant-table-row td {
-    padding-top: 15px;
-    padding-bottom: 15px;
-}
-
-.anty-row-operator button {
-    margin: 0 5px;
-}
-
-.ant-btn-danger {
-    background-color: #ffffff;
-}
-
-.ant-modal-cust-warp {
-    height: 100%;
-}
-
-.ant-modal-cust-warp .ant-modal-body {
-    height: calc(100% - 110px) !important;
-    overflow-y: auto;
-}
-
-.ant-modal-cust-warp .ant-modal-content {
-    height: 90% !important;
-    overflow-y: hidden;
-}
-/** Button按钮间距 */
-.ant-btn {
-    margin-left: 3px;
-}
+@import "~@assets/less/common.less";
 </style>
