f1c593ad63682d4b23734ba1da7f4b65bdc4e863
luopeihuan <luopeihuan@gzyouai.com>
2019-12-11 20:56:32 +0800

    整理自动化代码

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/IGameInfoService.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/IGameInfoService.java
@@ -1,13 +1,13 @@
 package org.jeecg.modules.game.service;
 
-import org.jeecg.modules.game.entity.GameInfo;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.jeecg.modules.game.entity.GameInfo;
 
 /**
- * @description 游戏信息
  * @author jeecg-boot
- * @date   2019-12-11
  * @version V1.0
+ * @description 游戏信息
+ * @date 2019-12-11
  */
 public interface IGameInfoService extends IService<GameInfo> {
 
--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/impl/GameInfoServiceImpl.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/impl/GameInfoServiceImpl.java
@@ -1,17 +1,16 @@
 package org.jeecg.modules.game.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.jeecg.modules.game.entity.GameInfo;
 import org.jeecg.modules.game.mapper.GameInfoMapper;
 import org.jeecg.modules.game.service.IGameInfoService;
 import org.springframework.stereotype.Service;
 
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-
 /**
- * @description 游戏信息
  * @author jeecg-boot
- * @date   2019-12-11
  * @version V1.0
+ * @description 游戏信息
+ * @date 2019-12-11
  */
 @Service
 public class GameInfoServiceImpl extends ServiceImpl<GameInfoMapper, GameInfo> implements IGameInfoService {
