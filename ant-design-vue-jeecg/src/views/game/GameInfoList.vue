91baa202d12c35f88f392222fd6d715a9e07c115
luopeihuan <luopeihuan@gzyouai.com>
2019-12-12 23:13:26 +0800

    去掉导入导出

--- ant-design-vue-jeecg/src/views/game/GameInfoList.vue
+++ ant-design-vue-jeecg/src/views/game/GameInfoList.vue
@@ -38,10 +38,10 @@
         <!-- 操作按钮区域 -->
         <div class="table-operator">
             <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>
-            <a-button type="primary" icon="download" @click="handleExportXls('游戏信息')">导出</a-button>
-            <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
+            <!-- <a-button type="primary" icon="download" @click="handleExportXls('游戏信息')">导出</a-button> -->
+            <!-- <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
                 <a-button type="primary" icon="import">导入</a-button>
-            </a-upload>
+            </a-upload> -->
             <a-dropdown v-if="selectedRowKeys.length > 0">
                 <a-menu slot="overlay">
                     <a-menu-item key="1" @click="batchDel"><a-icon type="delete" />删除</a-menu-item>
@@ -132,6 +132,11 @@ export default {
                     dataIndex: "name"
                 },
                 {
+                    title: "唯一标识",
+                    align: "center",
+                    dataIndex: "yaSimpleName"
+                },
+                {
                     title: "YA_APPID",
                     align: "center",
                     dataIndex: "yaAppId"
@@ -140,11 +145,6 @@ export default {
                     title: "YA_APPKEY",
                     align: "center",
                     dataIndex: "yaAppKey"
-                },
-                {
-                    title: "唯一标识",
-                    align: "center",
-                    dataIndex: "yaSimpleName"
                 },
                 {
                     title: "gameAppKey",
@@ -166,9 +166,9 @@ export default {
             url: {
                 list: "/game/gameInfo/list",
                 delete: "/game/gameInfo/delete",
-                deleteBatch: "/game/gameInfo/deleteBatch",
-                exportXlsUrl: "game/gameInfo/exportXls",
-                importExcelUrl: "game/gameInfo/importExcel"
+                deleteBatch: "/game/gameInfo/deleteBatch"
+                // exportXlsUrl: "game/gameInfo/exportXls",
+                // importExcelUrl: "game/gameInfo/importExcel"
             }
         };
     },
