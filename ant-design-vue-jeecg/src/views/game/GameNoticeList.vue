1382550eb0507a0dd29b71cc959dd00ea0ecfcf2
luopeihuan <luopeihuan@gzyouai.com>
2019-12-13 11:35:34 +0800

    添加游戏公告配置

---
+++ ant-design-vue-jeecg/src/views/game/GameNoticeList.vue
@@ -0,0 +1,194 @@
+<template>
+    <a-card :bordered="false">
+        <!-- 查询区域 -->
+        <div class="table-page-search-wrapper">
+            <a-form layout="inline" @keyup.enter.native="searchQuery">
+                <a-row :gutter="24">
+                    <a-col :md="6" :sm="8">
+                        <a-form-item label="公告类型 1 - 渠道公告 2 - 滚动公告">
+                            <a-input placeholder="请输入公告类型 1 - 渠道公告 2 - 滚动公告" v-model="queryParam.noticeType"></a-input>
+                        </a-form-item>
+                    </a-col>
+                    <a-col :md="6" :sm="8">
+                        <a-form-item label="标题">
+                            <a-input placeholder="请输入标题" v-model="queryParam.title"></a-input>
+                        </a-form-item>
+                    </a-col>
+                    <template v-if="toggleSearchStatus">
+                        <a-col :md="6" :sm="8">
+                            <a-form-item label="公告内容">
+                                <a-input placeholder="请输入公告内容" v-model="queryParam.content"></a-input>
+                            </a-form-item>
+                        </a-col>
+                        <a-col :md="6" :sm="8">
+                            <a-form-item label="开始时间">
+                                <a-input placeholder="请输入开始时间" v-model="queryParam.beginTime"></a-input>
+                            </a-form-item>
+                        </a-col>
+                        <a-col :md="6" :sm="8">
+                            <a-form-item label="结束时间">
+                                <a-input placeholder="请输入结束时间" v-model="queryParam.endTime"></a-input>
+                            </a-form-item>
+                        </a-col>
+                    </template>
+                    <a-col :md="6" :sm="8">
+                        <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
+                            <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
+                            <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
+                            <a @click="handleToggleSearch" style="margin-left: 8px">
+                                {{ toggleSearchStatus ? "收起" : "展开" }}
+                                <a-icon :type="toggleSearchStatus ? 'up' : 'down'" />
+                            </a>
+                        </span>
+                    </a-col>
+                </a-row>
+            </a-form>
+        </div>
+
+        <!-- 操作按钮区域 -->
+        <div class="table-operator">
+            <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>
+            <a-button type="primary" icon="download" @click="handleExportXls('游戏公告')">导出</a-button>
+            <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
+                <a-button type="primary" icon="import">导入</a-button>
+            </a-upload>
+            <a-dropdown v-if="selectedRowKeys.length > 0">
+                <a-menu slot="overlay">
+                    <a-menu-item key="1" @click="batchDel"><a-icon type="delete" />删除</a-menu-item>
+                </a-menu>
+                <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down"/></a-button>
+            </a-dropdown>
+        </div>
+
+        <!-- table区域-begin -->
+        <div>
+            <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
+                <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择 <a style="font-weight: 600">{{ selectedRowKeys.length }}</a>项
+                <a style="margin-left: 24px" @click="onClearSelected">清空</a>
+            </div>
+
+            <a-table
+                ref="table"
+                size="middle"
+                bordered
+                rowKey="id"
+                :columns="columns"
+                :dataSource="dataSource"
+                :pagination="ipagination"
+                :loading="loading"
+                :rowSelection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
+                @change="handleTableChange"
+            >
+                <span slot="action" slot-scope="text, record">
+                    <a @click="handleEdit(record)">编辑</a>
+
+                    <a-divider type="vertical" />
+                    <a-dropdown>
+                        <a class="ant-dropdown-link">更多 <a-icon type="down"/></a>
+                        <a-menu slot="overlay">
+                            <a-menu-item>
+                                <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">
+                                    <a>删除</a>
+                                </a-popconfirm>
+                            </a-menu-item>
+                        </a-menu>
+                    </a-dropdown>
+                </span>
+            </a-table>
+        </div>
+        <!-- table区域-end -->
+
+        <!-- 表单区域 -->
+        <gameNotice-modal ref="modalForm" @ok="modalFormOk"></gameNotice-modal>
+    </a-card>
+</template>
+
+<script>
+import { filterObj } from "@/utils/util";
+import { getAction, putAction, httpAction } from "@/api/manage";
+import GameNoticeModal from "./modules/GameNoticeModal";
+import { JeecgListMixin } from "@/mixins/JeecgListMixin";
+
+export default {
+    name: "GameNoticeList",
+    mixins: [JeecgListMixin],
+    components: {
+        GameNoticeModal
+    },
+    data() {
+        return {
+            description: "游戏公告管理页面",
+            // 表头
+            columns: [
+                {
+                    title: "#",
+                    dataIndex: "",
+                    key: "rowIndex",
+                    width: 60,
+                    align: "center",
+                    customRender: function(t, r, index) {
+                        return parseInt(index) + 1;
+                    }
+                },
+                {
+                    title: "公告类型 1 - 渠道公告 2 - 滚动公告",
+                    align: "center",
+                    dataIndex: "noticeType"
+                },
+                {
+                    title: "标题",
+                    align: "center",
+                    dataIndex: "title"
+                },
+                {
+                    title: "公告内容",
+                    align: "center",
+                    dataIndex: "content"
+                },
+                {
+                    title: "开始时间",
+                    align: "center",
+                    dataIndex: "beginTime"
+                },
+                {
+                    title: "结束时间",
+                    align: "center",
+                    dataIndex: "endTime"
+                },
+                {
+                    title: "状态 1 - 可用 0 - 不可用",
+                    align: "center",
+                    dataIndex: "status"
+                },
+                {
+                    title: "滚动公告间隔",
+                    align: "center",
+                    dataIndex: "interval"
+                },
+                {
+                    title: "操作",
+                    dataIndex: "action",
+                    align: "center",
+                    scopedSlots: { customRender: "action" },
+                }
+            ],
+            url: {
+                list: "/game/gameNotice/list",
+                delete: "/game/gameNotice/delete",
+                deleteBatch: "/game/gameNotice/deleteBatch",
+                exportXlsUrl: "game/gameNotice/exportXls",
+                importExcelUrl: "game/gameNotice/importExcel",
+            },
+        }
+    },
+    computed: {
+        importExcelUrl: function() {
+            return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`;
+        }
+    },
+    methods: {}
+}
+</script>
+<style scoped>
+@import "~@assets/less/common.less";
+</style>
