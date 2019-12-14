08826b4c58b4fd2a13205cf79426b9e86ce2d629
luopeihuan <luopeihuan@gzyouai.com>
2019-12-14 22:09:07 +0800

    添加玩家信息查询

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/player/mapper/PlayerInfoMapper.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.player.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.jeecg.modules.player.entity.PlayerInfo;
+
+/**
+ * @author jeecg-boot
+ * @version V1.0
+ * @description 玩家信息
+ * @date 2019-12-14
+ */
+public interface PlayerInfoMapper extends BaseMapper<PlayerInfo> {
+
+}
