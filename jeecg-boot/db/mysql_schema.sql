a4475fa2294f28a121cabe66272d0b07b51b3bd3
luopeihuan <luopeihuan@gzyouai.com>
2019-12-17 21:43:03 +0800

    添加数据库文件

--- jeecg-boot/db/mysql_schema.sql
+++ jeecg-boot/db/mysql_schema.sql
@@ -1,2 +1,2 @@
 -- 创建mysql库
-create database `` default character set utf8mb4 collate utf8mb4_general_ci;xiuzhen-admin
+create database `xiuzhen-admin` default character set utf8mb4 collate utf8mb4_general_ci;
