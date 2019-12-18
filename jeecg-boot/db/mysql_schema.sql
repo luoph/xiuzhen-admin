f079b33aa1cada3e045c628a340d0f0f471d5f72
luopeihuan <luopeihuan@gzyouai.com>
2019-12-18 10:17:32 +0800

    修改数据库信息

--- jeecg-boot/db/mysql_schema.sql
+++ jeecg-boot/db/mysql_schema.sql
@@ -1,2 +1,2 @@
 -- 创建mysql库
-create database `xiuzhen-admin` default character set utf8mb4 collate utf8mb4_general_ci;
+create database `game-center` default character set utf8mb4 collate utf8mb4_general_ci;
