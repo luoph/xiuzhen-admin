dba77d289d9a6e019c1a1043f1a5bb0761dee9e1
luopeihuan <luopeihuan@gzyouai.com>
2019-12-23 15:01:14 +0800

    修正html编辑器回显问题

--- jeecg-vue/src/components/jeecg/JEditor.vue
+++ jeecg-vue/src/components/jeecg/JEditor.vue
@@ -49,9 +49,9 @@ export default {
         return {
             // 初始化配置
             init: {
-                language_url: "tinymce/langs/zh_CN.js",
+                language_url: "/tinymce/langs/zh_CN.js",
                 language: "zh_CN",
-                skin_url: "tinymce/skins/lightgray",
+                skin_url: "/tinymce/skins/lightgray",
                 height: 300,
                 plugins: this.plugins,
                 toolbar: this.toolbar,
