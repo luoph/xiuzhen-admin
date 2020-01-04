a69960ae7914760b99d81d8d99361d5e40372509
luopeihuan <luopeihuan@gzyouai.com>
2020-01-04 15:37:15 +0800

    添加游戏设置

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/mapper/GameSettingMapper.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.game.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.jeecg.modules.game.entity.GameSetting;
+
+/**
+ * @author jeecg-boot
+ * @version V1.0
+ * @description 游戏设置
+ * @date 2020-01-04
+ */
+public interface GameSettingMapper extends BaseMapper<GameSetting> {
+
+}
---
+++ jeecg-vue/src/views/game/modules/GameSettingModal.vue
@@ -0,0 +1,129 @@
+<template>
+    <a-drawer :title="title" :width="width" placement="right" :closable="false" @close="close" :visible="visible">
+    <!-- <a-modal :title="title" :width="width" :visible="visible" :confirmLoading="confirmLoading" @ok="handleOk" @cancel="handleCancel" cancelText="关闭"> -->
+        <a-spin :spinning="confirmLoading">
+            <a-form :form="form">
+                    <a-form-item label="key" :labelCol="labelCol" :wrapperCol="wrapperCol">
+                    <a-input :disabled="isEdit" v-decorator="['dictKey', validatorRules.dictKey]" placeholder="请输入key"></a-input>
+                </a-form-item>
+                <a-form-item label="value" :labelCol="labelCol" :wrapperCol="wrapperCol">
+                    <a-input v-decorator="['dictValue', validatorRules.dictValue]" placeholder="请输入value"></a-input>
+                </a-form-item>
+                <a-form-item label="描述" :labelCol="labelCol" :wrapperCol="wrapperCol">
+                    <a-input v-decorator="['remark', validatorRules.remark]" placeholder="请输入描述"></a-input>
+                </a-form-item>
+            </a-form>
+        </a-spin>
+    <!-- </a-modal> -->
+        <a-button type="primary" @click="handleOk">确定</a-button>
+        <a-button type="primary" @click="handleCancel">取消</a-button>
+    </a-drawer>
+</template>
+
+<script>
+import { httpAction } from "@/api/manage";
+import pick from "lodash.pick";
+
+export default {
+    name: "GameSettingModal",
+    components: {
+    },
+    data() {
+        return {
+            form: this.$form.createForm(this),
+            title: "操作",
+            isEdit: false,
+            width: 800,
+            visible: false,
+            model: {},
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 5 }
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 16 }
+            },
+            confirmLoading: false,
+            validatorRules: {
+                dictKey: { rules: [{ required: true, message: "请输入key!" }] },
+                dictValue: { rules: [{ required: true, message: "请输入value!" }] },
+                remark: { rules: [{ required: true, message: "请输入描述!" }] },
+            },
+            url: {
+                add: "game/gameSetting/add",
+                edit: "game/gameSetting/edit"
+            }
+        };
+    },
+    created() {
+    },
+    methods: {
+        add() {
+            this.edit({});
+        },
+        edit(record) {
+            this.form.resetFields();
+            this.model = Object.assign({}, record);
+            this.isEdit = this.model.id != null;
+            this.visible = true;
+            this.$nextTick(() => {
+                this.form.setFieldsValue(pick(this.model, "dictKey", "dictValue", "remark"));
+            });
+        },
+        close() {
+            this.$emit("close");
+            this.visible = false;
+        },
+        handleOk() {
+            const that = this;
+            // 触发表单验证
+            this.form.validateFields((err, values) => {
+                if (!err) {
+                    that.confirmLoading = true;
+                    let httpUrl = "";
+                    let method = "";
+                    if (!this.model.id) {
+                        httpUrl += this.url.add;
+                        method = "post";
+                    } else {
+                        httpUrl += this.url.edit;
+                        method = "put";
+                    }
+                    let formData = Object.assign(this.model, values);
+                    console.log("表单提交数据", formData);
+                    httpAction(httpUrl, formData, method)
+                        .then(res => {
+                            if (res.success) {
+                                that.$message.success(res.message);
+                                that.$emit("ok");
+                            } else {
+                                that.$message.warning(res.message);
+                            }
+                        })
+                        .finally(() => {
+                            that.confirmLoading = false;
+                            that.close();
+                        });
+                }
+            });
+        },
+        handleCancel() {
+            this.close();
+        },
+        popupCallback(row) {
+            this.form.setFieldsValue(pick(row, "dictKey", "dictValue", "remark"));
+        },
+    }
+};
+</script>
+
+// <style lang="less" scoped></style>
+<style lang="less" scoped>
+/** Button按钮间距 */
+.ant-btn {
+    margin-left: 30px;
+    margin-bottom: 30px;
+    float: right;
+}
+</style>
