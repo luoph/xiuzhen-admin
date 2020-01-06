74b04f4ef9941fc583cf4752b48bcc4ac45091b9
luopeihuan <luopeihuan@gzyouai.com>
2020-01-07 01:38:43 +0800

    优化兑换码编辑页面

--- jeecg-vue/src/views/game/RedeemActivityList.vue
+++ jeecg-vue/src/views/game/RedeemActivityList.vue
@@ -47,12 +47,12 @@
             <!-- <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
                 <a-button type="primary" icon="import">导入</a-button>
             </a-upload> -->
-            <a-dropdown v-if="selectedRowKeys.length > 0">
+            <!-- <a-dropdown v-if="selectedRowKeys.length > 0">
                 <a-menu slot="overlay">
                     <a-menu-item key="1" @click="batchDel"><a-icon type="delete" />删除</a-menu-item>
                 </a-menu>
                 <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down"/></a-button>
-            </a-dropdown>
+            </a-dropdown> -->
         </div>
 
         <!-- table区域-begin -->
@@ -72,7 +72,6 @@
                 :dataSource="dataSource"
                 :pagination="ipagination"
                 :loading="loading"
-                :rowSelection="{ fixed: true, selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
                 @change="handleTableChange"
             >
                 <template slot="htmlSlot" slot-scope="text">
@@ -89,7 +88,7 @@
 
                 <span slot="action" slot-scope="text, record">
                     <a @click="handleEdit(record)">编辑</a>
-                    <a-divider type="vertical" />
+                    <!-- <a-divider type="vertical" />
                     <a-dropdown>
                         <a class="ant-dropdown-link">更多 <a-icon type="down"/></a>
                         <a-menu slot="overlay">
@@ -99,7 +98,7 @@
                                 </a-popconfirm>
                             </a-menu-item>
                         </a-menu>
-                    </a-dropdown>
+                    </a-dropdown> -->
                 </span>
             </a-table>
         </div>
