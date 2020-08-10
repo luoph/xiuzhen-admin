95b34474fc65c4bcff6d5a3671c30aa63b2c3efc
luopeihuan <luopeihuan@gzyouai.com>
2020-08-10 18:32:14 +0800

    去掉操作栏

--- jeecg-vue/src/views/player/PlayerInfoList.vue
+++ jeecg-vue/src/views/player/PlayerInfoList.vue
@@ -42,17 +42,17 @@
         <!-- 查询区域-END -->
         <!-- 操作按钮区域 -->
         <div class="table-operator">
-            <a-button type="primary" icon="plus" @click="handleAdd">新增</a-button>
+            <!-- <a-button type="primary" icon="plus" @click="handleAdd">新增</a-button> -->
             <a-button type="primary" icon="download" @click="handleExportXls('玩家信息')">导出</a-button>
-            <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
+            <!-- <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
                 <a-button type="primary" icon="import">导入</a-button>
-            </a-upload>
-            <a-dropdown v-if="selectedRowKeys.length > 0">
+            </a-upload> -->
+            <!-- <a-dropdown v-if="selectedRowKeys.length > 0">
                 <a-menu slot="overlay">
                     <a-menu-item key="1" @click="batchDel"><a-icon type="delete" />删除</a-menu-item>
                 </a-menu>
                 <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down"/></a-button>
-            </a-dropdown>
+            </a-dropdown> -->
         </div>
 
         <!-- table区域-begin -->
