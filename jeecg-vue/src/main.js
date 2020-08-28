54923a8117895a4a86d752d51f5d9fe48a2a009a
buliangliang <buliangliang@gzyouai.com>
2020-08-28 18:01:54 +0800

    样式被覆盖问题
    样式被覆盖问题

--- jeecg-vue/src/main.js
+++ jeecg-vue/src/main.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
-import App from "./App.vue";
 import Storage from "vue-ls";
-import router from "./router";
 import store from "./store/";
+import App from "./App.vue";
+import router from "./router";
 
 import { VueAxios } from "@/utils/request";
 
