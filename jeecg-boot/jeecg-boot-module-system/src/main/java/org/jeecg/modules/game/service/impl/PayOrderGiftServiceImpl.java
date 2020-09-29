cc5a29bf801c425ffeda6d4716983ea4fc1935a7
xubodong <xubodong@gzyouai.com>
2020-09-29 20:12:35 +0800

    礼包消费统计接口完善
    礼包消费统计接口完善

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/impl/PayOrderGiftServiceImpl.java
@@ -0,0 +1,21 @@
+package org.jeecg.modules.game.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.jeecg.modules.game.entity.PayOrderGift;
+import org.jeecg.modules.game.entity.PayOrderGiftVO;
+import org.jeecg.modules.game.mapper.PayOrderGiftMapper;
+import org.jeecg.modules.game.service.IPayOrderGiftService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author jeecg-boot
+ * @version V1.0
+ * @description 消费礼包数据统计
+ * @date 2020-09-29
+ */
+@Service
+public class PayOrderGiftServiceImpl extends ServiceImpl<PayOrderGiftMapper, PayOrderGift> implements IPayOrderGiftService {
+
+}
