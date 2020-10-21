9412a35f160b0be3474e92ce3490d5868bc86751
luopeihuan <luopeihuan@gzyouai.com>
2020-10-21 19:54:06 +0800

    去掉 userOnlineRecord 关联

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/player/entity/PlayerRegisterInfo.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/player/entity/PlayerRegisterInfo.java
@@ -1,6 +1,7 @@
 package org.jeecg.modules.player.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -158,5 +159,6 @@ public class PlayerRegisterInfo implements Serializable {
     /**
      * 用户在线时长统计对象
      */
+    @TableField(exist = false)
     private UserOnlineRecord userOnlineRecord;
 }
