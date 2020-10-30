fc9d1c03817e390d7ba1e023bdddc9bcbe854291
luopeihuan <luopeihuan@gzyouai.com>
2020-10-30 15:14:39 +0800

    新增页签活动配置详情下发

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/constant/CampaignFestivalType.java
@@ -0,0 +1,45 @@
+package org.jeecg.modules.game.constant;
+
+import lombok.Getter;
+import org.jeecg.modules.game.entity.*;
+
+/**
+ * 节日活动类型
+ * 1.登录礼包, 2.累计充值, 3.兑换, 4.节日任务, 5.buff-修为加成, 6.buff-灵气加成
+ *
+ * @author luopeihuan
+ */
+@Getter
+@SuppressWarnings("rawtypes")
+public enum CampaignFestivalType {
+
+    /**
+     * 活动状态
+     */
+    LOGIN("登录礼包", 1, GameCampaignTypeLogin.class),
+    RECHARGE("累计充值", 2, GameCampaignTypeRecharge.class),
+    EXCHANGE("兑换", 3, GameCampaignTypeExchange.class),
+    TASK("节日任务", 4, GameCampaignTypeTask.class),
+    BUFF_PRACTICE("修为加成", 5, GameCampaignTypeBuff.class),
+    BUFF_ANIMA("灵气加成", 6, GameCampaignTypeBuff.class),
+    ;
+
+    private final String name;
+    private final int value;
+    private final Class tableClass;
+
+    CampaignFestivalType(String name, int value, Class tableClass) {
+        this.name = name;
+        this.value = value;
+        this.tableClass = tableClass;
+    }
+
+    public static CampaignFestivalType valueOf(int type) {
+        for (CampaignFestivalType e : CampaignFestivalType.values()) {
+            if (e.getValue() == type) {
+                return e;
+            }
+        }
+        return null;
+    }
+}
--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/entity/GameCampaignTypeBuff.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/entity/GameCampaignTypeBuff.java
@@ -45,7 +45,7 @@ public class GameCampaignTypeBuff extends BaseEntity {
     private java.lang.Long typeId;
 
     /**
-     * 活动项类型: 1.登录礼包, 2.累计充值, 3.兑换 4.节日任务, 5.buff-修为加成, 6.buff-灵所加成
+     * 活动项类型: 1.登录礼包, 2.累计充值, 3.兑换 4.节日任务, 5.buff-修为加成, 6.buff-灵气加成
      */
     @Excel(name = "活动项类型", width = 15)
     private java.lang.Integer type;
