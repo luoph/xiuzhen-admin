1e1f75ac694bd21c0b3d2c92cc5bafd2d33c9579
luopeihuan <luopeihuan@gzyouai.com>
2020-11-03 00:01:53 +0800

    新增图片管理

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/entity/GameImage.java
@@ -0,0 +1,67 @@
+package org.jeecg.modules.game.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import org.jeecg.common.system.base.entity.BaseEntity;
+import org.jeecgframework.poi.excel.annotation.Excel;
+
+/**
+ * @author jeecg-boot
+ * @version V1.0
+ * @description 游戏图片
+ * @date 2020-11-02
+ */
+@Data
+@TableName("game_image")
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+public class GameImage extends BaseEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 活动id
+     */
+    @TableId(type = IdType.AUTO)
+    private java.lang.Integer id;
+
+    /**
+     * 图片类型: 1.icon, 2.banner
+     */
+    @Excel(name = "图片类型", width = 15)
+    private java.lang.Integer type;
+
+    /**
+     * 图片名
+     */
+    @Excel(name = "图片名", width = 15)
+    private java.lang.String name;
+
+    /**
+     * 相对路径
+     */
+    @Excel(name = "相对路径", width = 15)
+    private java.lang.String url;
+
+    /**
+     * 图片宽px
+     */
+    @Excel(name = "图片宽px", width = 15)
+    private java.lang.Integer width;
+
+    /**
+     * 图片高px
+     */
+    @Excel(name = "图片高px", width = 15)
+    private java.lang.Integer height;
+
+    /**
+     * 备注
+     */
+    @Excel(name = "备注", width = 15)
+    private java.lang.String remark;
+}
---
+++ jeecg-vue/src/views/game/GameImageList.vue
@@ -0,0 +1,201 @@
+<template>
+    <a-card :bordered="false">
+        <!-- 查询区域 -->
+        <div class="table-page-search-wrapper">
+            <a-form layout="inline" @keyup.enter.native="searchQuery">
+                <a-row :gutter="24">
+                    <a-col :md="6" :sm="8">
+                        <a-form-item label="图片类型">
+                            <a-input placeholder="请输入图片类型" v-model="queryParam.type"></a-input>
+                        </a-form-item>
+                    </a-col>
+                    <a-col :md="6" :sm="8">
+                        <a-form-item label="图片名">
+                            <a-input placeholder="请输入图片名" v-model="queryParam.name"></a-input>
+                        </a-form-item>
+                    </a-col>
+                    <template v-if="toggleSearchStatus">
+                        <a-col :md="6" :sm="8">
+                            <a-form-item label="相对路径">
+                                <a-input placeholder="请输入相对路径" v-model="queryParam.url"></a-input>
+                            </a-form-item>
+                        </a-col>
+                    </template>
+                    <a-col :md="6" :sm="8">
+                        <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
+                            <a-button type="primary" icon="search" @click="searchQuery">查询</a-button>
+                            <a-button type="primary" icon="reload" style="margin-left: 8px" @click="searchReset">重置</a-button>
+                            <a style="margin-left: 8px" @click="handleToggleSearch">
+                                {{ toggleSearchStatus ? "收起" : "展开" }}
+                                <a-icon :type="toggleSearchStatus ? 'up' : 'down'" />
+                            </a>
+                        </span>
+                    </a-col>
+                </a-row>
+            </a-form>
+        </div>
+        <!-- 查询区域-END -->
+        <!-- 操作按钮区域 -->
+        <div class="table-operator">
+            <a-button type="primary" icon="plus" @click="handleAdd">新增</a-button>
+            <a-button type="primary" icon="download" @click="handleExportXls('游戏图片')">导出</a-button>
+            <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
+                <a-button type="primary" icon="import">导入</a-button>
+            </a-upload>
+            <a-dropdown v-if="selectedRowKeys.length > 0">
+                <a-menu slot="overlay">
+                    <a-menu-item key="1" @click="batchDel"><a-icon type="delete" />删除</a-menu-item>
+                </a-menu>
+                <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down"/></a-button>
+            </a-dropdown>
+        </div>
+
+        <!-- table区域-begin -->
+        <div>
+            <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
+                <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择 <a style="font-weight: 600">{{ selectedRowKeys.length }}</a
+                >项
+                <a style="margin-left: 24px" @click="onClearSelected">清空</a>
+            </div>
+
+            <a-table
+                ref="table"
+                size="middle"
+                bordered
+                rowKey="id"
+                :columns="columns"
+                :dataSource="dataSource"
+                :pagination="ipagination"
+                :loading="loading"
+                :rowSelection="{ fixed: true, selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
+                @change="handleTableChange"
+            >
+                <template slot="htmlSlot" slot-scope="text">
+                    <div v-html="text"></div>
+                </template>
+                <template slot="imgSlot" slot-scope="text">
+                    <span v-if="!text" style="font-size: 12px;font-style: italic;">无此图片</span>
+                    <img v-else :src="getImgView(text)" height="25px" alt="图片不存在" style="max-width:80px;font-size: 12px;font-style: italic;" />
+                </template>
+                <template slot="fileSlot" slot-scope="text">
+                    <span v-if="!text" style="font-size: 12px;font-style: italic;">无此文件</span>
+                    <a-button v-else :ghost="true" type="primary" icon="download" size="small" @click="uploadFile(text)"> 下载 </a-button>
+                </template>
+
+                <span slot="action" slot-scope="text, record">
+                    <a @click="handleEdit(record)">编辑</a>
+                    <a-divider type="vertical" />
+                    <a-dropdown>
+                        <a class="ant-dropdown-link">更多 <a-icon type="down"/></a>
+                        <a-menu slot="overlay">
+                            <a-menu-item>
+                                <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">
+                                    <a>删除</a>
+                                </a-popconfirm>
+                            </a-menu-item>
+                        </a-menu>
+                    </a-dropdown>
+                </span>
+            </a-table>
+        </div>
+
+        <gameImage-modal ref="modalForm" @ok="modalFormOk"></gameImage-modal>
+    </a-card>
+</template>
+
+<script>
+import { JeecgListMixin } from "@/mixins/JeecgListMixin";
+import GameImageModal from "./modules/GameImageModal";
+
+export default {
+    name: "GameImageList",
+    mixins: [JeecgListMixin],
+    components: {
+        GameImageModal
+    },
+    data() {
+        return {
+            description: "游戏图片管理页面",
+            // 表头
+            columns: [
+                {
+                    title: "#",
+                    dataIndex: "",
+                    key: "rowIndex",
+                    width: 60,
+                    align: "center",
+                    customRender: function(t, r, index) {
+                        return parseInt(index) + 1;
+                    }
+                },
+                {
+                    title: "图片类型",
+                    align: "center",
+                    dataIndex: "type",
+                },
+                {
+                    title: "图片名",
+                    align: "center",
+                    dataIndex: "name"
+                },
+                {
+                    title: "相对路径",
+                    align: "center",
+                    dataIndex: "url"
+                },
+                {
+                    title: "图片宽px",
+                    align: "center",
+                    dataIndex: "width"
+                },
+                {
+                    title: "图片高px",
+                    align: "center",
+                    dataIndex: "height"
+                },
+                {
+                    title: "备注",
+                    align: "center",
+                    dataIndex: "remark"
+                },
+                {
+                    title: "createTime",
+                    align: "center",
+                    dataIndex: "createTime"
+                },
+                {
+                    title: "创建人",
+                    align: "center",
+                    dataIndex: "createBy"
+                },
+                {
+                    title: "操作",
+                    dataIndex: "action",
+                    align: "center",
+                    scopedSlots: { customRender: "action" }
+                }
+            ],
+            url: {
+                list: "game/gameImage/list",
+                delete: "game/gameImage/delete",
+                deleteBatch: "game/gameImage/deleteBatch",
+                exportXlsUrl: "game/gameImage/exportXls",
+                importExcelUrl: "game/gameImage/importExcel"
+            },
+            dictOptions: {}
+        };
+    },
+    computed: {
+        importExcelUrl: function() {
+            return `${window._CONFIG["domianURL"]}/${this.url.importExcelUrl}`;
+        }
+    },
+    methods: {
+        initDictConfig() {},
+    }
+};
+</script>
+
+<style scoped>
+@import "~@assets/less/common.less";
+</style>
