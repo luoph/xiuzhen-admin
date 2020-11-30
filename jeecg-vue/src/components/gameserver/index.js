4c6d688f5493b1c97331096cafb624a79532dc19
buliangliang <buliangliang@gzyouai.com>
2020-11-30 17:45:33 +0800

    自定义json文件下拉框组件

--- jeecg-vue/src/components/gameserver/index.js
+++ jeecg-vue/src/components/gameserver/index.js
@@ -1,30 +1,38 @@
 /** 自定义全局组件 */
-import ServerSelect from './ServerSelect.vue'
-import MultipleServerSelect from './MultipleServerSelect.vue'
-import GameChannelServer from './GameChannelServer.vue'
+import ServerSelect from "./ServerSelect.vue";
+import MultipleServerSelect from "./MultipleServerSelect.vue";
+import GameChannelServer from "./GameChannelServer.vue";
+import ASelectReadJson from "./ASelectReadJson.vue";
 
 const ServerSelectList = {
     install: function(Vue) {
-        Vue.component('ServerSelect', ServerSelect);
+        Vue.component("ServerSelect", ServerSelect);
     }
 };
 
 const MultipleServerSelectList = {
     install: function(Vue) {
-        Vue.component('MultipleSeverSelect', MultipleServerSelect);
+        Vue.component("MultipleSeverSelect", MultipleServerSelect);
     }
 };
 
 const GameChannelServerList = {
     install: function(Vue) {
-        Vue.component('GameChannelServer', GameChannelServer);
+        Vue.component("GameChannelServer", GameChannelServer);
     }
-}
+};
+
+const ASelectReadJsonList = {
+    install: function(Vue) {
+        Vue.component("ASelectReadJson", ASelectReadJson);
+    }
+};
 
 export default () => {
     return {
         ServerSelectList,
         MultipleServerSelectList,
         GameChannelServerList,
+        ASelectReadJsonList
     };
 };
