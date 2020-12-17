eab907eaa8eb3cb1d748001d9f07cf73dc4d8457
huli <yw15397919336@163.com>
2020-12-17 20:34:38 +0800

    新增封号禁言模块

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/mapper/GameForbiddenRecordMapper.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.game.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.jeecg.modules.game.entity.GameForbiddenRecord;
+
+/**
+ * @author jeecg-boot
+ * @version V1.0
+ * @description game_forbidden_record
+ * @date 2020-12-17
+ */
+public interface GameForbiddenRecordMapper extends BaseMapper<GameForbiddenRecord> {
+
+}
---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/impl/GameForbiddenServiceImpl.java
@@ -0,0 +1,18 @@
+package org.jeecg.modules.game.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.jeecg.modules.game.entity.GameForbidden;
+import org.jeecg.modules.game.mapper.GameForbiddenMapper;
+import org.jeecg.modules.game.service.IGameForbiddenService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author jeecg-boot
+ * @version V1.0
+ * @description game_forbidden
+ * @date 2020-12-17
+ */
+@Service
+public class GameForbiddenServiceImpl extends ServiceImpl<GameForbiddenMapper, GameForbidden> implements IGameForbiddenService {
+
+}
