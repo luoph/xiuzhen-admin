7f4217231c38da303e5889300add1314e7aa405f
huli <yw15397919336@163.com>
2020-12-23 14:29:34 +0800

    留存统计：新增留存、首付留存、免费留存、分档位留存

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/mapper/RemainStatisticsMapper.java
@@ -0,0 +1,67 @@
+package org.jeecg.modules.game.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.jeecg.modules.game.entity.GameRemainStatistisc;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * author:huli
+ * description:留存统计mapper
+ */
+public interface RemainStatisticsMapper extends BaseMapper<GameRemainStatistisc> {
+    /**
+     * 时间段内存在于 注册表 也存在于 登录表 的数量
+     * @param tableName
+     * @param registerBeginTime
+     * @param registerEndTime
+     * @param channelName
+     * @param serverId
+     * @param loginBeginTime
+     * @param loginEndTime
+     * @return
+     */
+    @Select("select count(*) as ramainSum from (" +
+            "SELECT count(*) FROM game_register_info a LEFT JOIN ${tableName} l " +
+            "ON l.`player_id` = a.`player_id` AND l.`channel` = a.`channel` AND l.`server_id` = a.`server_id` " +
+            "WHERE l.`create_time` >= STR_TO_DATE(#{loginBeginTime},'%Y-%m-%d %H:%i:%s') " +
+            "AND l.`create_time` < STR_TO_DATE(#{loginEndTime},'%Y-%m-%d %H:%i:%s') " +
+            "AND a.`create_time` >= STR_TO_DATE(#{registerBeginTime},'%Y-%m-%d %H:%i:%s') " +
+            "AND a.`create_time` < STR_TO_DATE(#{registerEndTime},'%Y-%m-%d %H:%i:%s') " +
+            "AND a.`channel` = #{channelName} AND a.`server_id` = #{serverId} AND l.`channel` = #{channelName}" +
+            "AND l.`server_id` = #{serverId} GROUP BY a.player_id" +
+            ") b")
+    String  selectnNwUserRemainSum(@Param("tableName") String tableName,@Param("registerBeginTime") String registerBeginTime,
+                                     @Param("registerEndTime") String registerEndTime,@Param("channelName") String channelName,
+                                     @Param("serverId") int serverId,@Param("loginBeginTime") String loginBeginTime,@Param("loginEndTime") String loginEndTime);
+
+
+    /**
+     * 查询时间范围内所有注册的用户
+     */
+    @Select("select account,create_date,player_id from game_register_info where channel = #{channel} and server_id = #{serverId} and create_date >= STR_TO_DATE(#{creatTimeBegin},'%Y-%m-%d') and create_date <= STR_TO_DATE(#{creatTimeEnd},'%Y-%m-%d') ")
+    List<Map> selectAllRegisterUser(@Param("channel") String channel,@Param("serverId") int serverId, @Param("creatTimeBegin") String creatTimeBegin,@Param("creatTimeEnd") String creatTimeEnd);
+
+    /**
+     * 查询时间范围内所有登陆操作的用户
+     */
+    @Select("select account,create_date,player_id from ${tableName} where channel = #{channel} and server_id = #{serverId} and create_date >= STR_TO_DATE(#{creatTimeBegin},'%Y-%m-%d') and create_date <= STR_TO_DATE(#{creatTimeEnd},'%Y-%m-%d') ")
+    List<Map> selectAllLoginAndRegisterUser(@Param("channel") String channel,@Param("serverId") int serverId, @Param("tableName") String tableName,@Param("creatTimeBegin") String creatTimeBegin,@Param("creatTimeEnd") String creatTimeEnd);
+
+    /**
+     * 查询时间范围内所有已支付订单表
+     */
+    @Select("select player_id,pay_amount,create_time from game_order where channel = #{channelName} and server_id = #{serverId} and  order_status >= 1 and create_time >= STR_TO_DATE(#{creatTimeBegin},'%Y-%m-%d %H:%i:%s') and create_time <= STR_TO_DATE(#{creatTimeEnd},'%Y-%m-%d %H:%i:%s') ")
+    List<Map> selectAllOrderHasPayList(@Param("channelName") String channelName, @Param("serverId") int serverId, @Param("creatTimeBegin") String creatTimeBegin, @Param("creatTimeEnd") String creatTimeEnd);
+
+    /**
+     * 查询时间范围内所有未支付订单表
+     */
+    @Select("select player_id,pay_amount,create_time from game_order where channel = #{channelName} and server_id = #{serverId} and  order_status = 0 and create_time >= STR_TO_DATE(#{creatTimeBegin},'%Y-%m-%d %H:%i:%s') and create_time <= STR_TO_DATE(#{creatTimeEnd},'%Y-%m-%d %H:%i:%s') ")
+    List<Map> selectAllOrderNoPayList(@Param("channelName") String channelName, @Param("serverId") int serverId, @Param("creatTimeBegin") String creatTimeBegin, @Param("creatTimeEnd") String creatTimeEnd);
+
+}
---
+++ jeecg-vue/src/views/game/GameRemainOfDownPaymentList.vue
@@ -0,0 +1,254 @@
+<template>
+    <a-card :bordered="false">
+        <!-- 查询区域 -->
+        <div class="table-page-search-wrapper">
+            <a-form layout="inline" @keyup.enter.native="searchQuery">
+                <a-row :gutter="45">
+                    <a-col :md="10" :sm="8">
+                        <!--@ = v-on:数据绑定 不是事件-->
+                        <game-channel-server @onSelectChannel="onSelectChannel" @onSelectServer="onSelectServer"></game-channel-server>
+                    </a-col>
+                    <a-col :md="10" :sm="8">
+                        <a-form-item label="创建日期">
+                            <a-range-picker format="YYYY-MM-DD" :placeholder="['开始日期', '结束日期']" @change="onDateChange" />
+                        </a-form-item>
+                    </a-col>
+                    <a-col :md="5" :sm="5">
+                        <a-form-item label="选择就近天数">
+                            <a-select placeholder="天数" v-model="queryParam.days">
+                                <a-select-option :value="0">不选择天数</a-select-option>
+                                <a-select-option :value="7">近7天</a-select-option>
+                                <a-select-option :value="15">近15天</a-select-option>
+                                <a-select-option :value="30">近一个月</a-select-option>
+                                <a-select-option :value="60">近两个月</a-select-option>
+                                <a-select-option :value="90">近90天</a-select-option>
+                                <a-select-option :value="120">近120天</a-select-option>
+                            </a-select>
+                        </a-form-item>
+                    </a-col>
+                    <a-col :md="4" :sm="8">
+                        <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
+                            <a-button type="primary" icon="search" @click="searchQuery">查询</a-button>
+                        </span>
+                    </a-col>
+                </a-row>
+            </a-form>
+        </div>
+        <!--查询区域结束-->
+        <!-- table区域-begin -->
+        <div>
+            <a-table
+                ref="table"
+                size="middle"
+                bordered
+                :rowKey="record => (record.id != null ? record.id : '0')"
+                :loading="loading"
+                :columns="columns"
+                :dataSource="dataSource"
+                :pagination="ipagination"
+                :scroll="{ x: 'max-content' }"
+                @change="handleTableChange"
+            ></a-table>
+        </div>
+    </a-card>
+</template>
+
+<script>
+import { JeecgListMixin } from "@/mixins/JeecgListMixin";
+import JDate from "@/components/jeecg/JDate.vue";
+import GameChannelServer from "@/components/gameserver/GameChannelServer";
+import { getAction } from "@/api/manage";
+
+export default {
+    description: "首付留存",
+    name: "GameRemainOfDownPaymentList",
+    mixins: [JeecgListMixin],
+    components: {
+        JDate,
+        GameChannelServer,
+        getAction
+    },
+    data() {
+        return {
+            columns: [
+                {
+                    title: "#",
+                    dataIndex: "",
+                    width: "100",
+                    align: "center",
+                    customRender: function(t, r, index) {
+                        return parseInt(index) + 1;
+                    }
+                },
+                {
+                    title: "日期",
+                    dataIndex: "countDate",
+                    width: "120",
+                    align: "center",
+                    customRender: function(text) {
+                        return !text ? "" : text.length > 10 ? text.substr(0, 10) : text;
+                    }
+                },
+                {
+                    title: "新增玩家",
+                    dataIndex: "registerNum",
+                    align: "center",
+                    width: "120"
+                },
+                {
+                    title: "2日留存",
+                    dataIndex: "c2",
+                    align: "center",
+                    width: "120",
+                    customRender: (text, record) => {
+                        return this.countRate(record.c2, record.registerNum);
+                    }
+                },
+                {
+                    title: "3日留存",
+                    dataIndex: "c3",
+                    align: "center",
+                    width: "120",
+                    customRender: (text, record) => {
+                        return this.countRate(record.c3, record.registerNum);
+                    }
+                },
+                {
+                    title: "4日留存",
+                    dataIndex: "c4",
+                    align: "center",
+                    width: "120",
+                    customRender: (text, record) => {
+                        return this.countRate(record.c4, record.registerNum);
+                    }
+                },
+                {
+                    title: "5日留存",
+                    dataIndex: "c5",
+                    align: "center",
+                    width: "120",
+                    customRender: (text, record) => {
+                        return this.countRate(record.c5, record.registerNum);
+                    }
+                },
+                {
+                    title: "6日留存",
+                    dataIndex: "c6",
+                    align: "center",
+                    width: "120",
+                    customRender: (text, record) => {
+                        return this.countRate(record.c6, record.registerNum);
+                    }
+                },
+                {
+                    title: "7日留存",
+                    dataIndex: "c7",
+                    align: "center",
+                    width: "120",
+                    customRender: (text, record) => {
+                        return this.countRate(record.c7, record.registerNum);
+                    }
+                },
+                {
+                    title: "15日留存",
+                    dataIndex: "c15",
+                    align: "center",
+                    width: "120",
+                    customRender: (text, record) => {
+                        return this.countRate(record.c15, record.registerNum);
+                    }
+                },
+                {
+                    title: "30日留存",
+                    dataIndex: "c30",
+                    align: "center",
+                    width: "120",
+                    customRender: (text, record) => {
+                        return this.countRate(record.c30, record.registerNum);
+                    }
+                },
+                {
+                    title: "60日留存",
+                    dataIndex: "c60",
+                    align: "center",
+                    width: "120",
+                    customRender: (text, record) => {
+                        return this.countRate(record.c60, record.registerNum);
+                    }
+                },
+                {
+                    title: "90日留存",
+                    dataIndex: "c90",
+                    align: "center",
+                    width: "120",
+                    customRender: (text, record) => {
+                        return this.countRate(record.c90, record.registerNum);
+                    }
+                },
+                {
+                    title: "120日留存",
+                    dataIndex: "c120",
+                    align: "center",
+                    width: "120",
+                    customRender: (text, record) => {
+                        return this.countRate(record.c120, record.registerNum);
+                    }
+                }
+            ],
+            url: {
+                list: "game/remainStatistisc/downPayment"
+            },
+            dictOptions: {}
+        };
+    },
+    computed: {},
+    methods: {
+        initDictConfig() {},
+        onSelectChannel: function(channelId) {
+            this.queryParam.channelId = channelId;
+        },
+        onSelectServer: function(serverId) {
+            this.queryParam.serverId = serverId;
+        },
+        onDateChange: function(value, dateStr) {
+            this.queryParam.rangeDateBegin = dateStr[0];
+            this.queryParam.rangeDateEnd = dateStr[1];
+        },
+        searchQuery() {
+            let param = {
+                days: this.queryParam.days,
+                channelId: this.queryParam.channelId,
+                serverId: this.queryParam.serverId,
+                rangeDateBegin: this.queryParam.rangeDateBegin,
+                rangeDateEnd: this.queryParam.rangeDateEnd,
+                pageNo: this.ipagination.current,
+                pageSize: this.ipagination.pageSize,
+                type: this.queryParam.type
+            };
+            getAction(this.url.list, param).then(res => {
+                if (res.success) {
+                    this.dataSource = res.result.records;
+                    this.ipagination.current = res.result.current;
+                    this.ipagination.size = res.result.size.toString();
+                    this.ipagination.total = res.result.total;
+                    this.ipagination.pages = res.result.pages;
+                } else {
+                    this.$message.error(res.message);
+                }
+            });
+        },
+        countRate: function(n, r) {
+            if (n === null || n === undefined) {
+                return "--";
+            }
+
+            let rate = r > 0 ? parseFloat(n / r) : 0;
+            return Number(parseFloat(rate * 100).toFixed(2)) + "%";
+        }
+    }
+};
+</script>
+
+<style scoped>
+@import "~@assets/less/common.less";
+</style>
