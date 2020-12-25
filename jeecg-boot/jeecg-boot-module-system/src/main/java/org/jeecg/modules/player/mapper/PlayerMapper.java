286d5377a72740d480c9d14e007512347e9c2ab8
huli <yw15397919336@163.com>
2020-12-25 15:28:01 +0800

    优化玩家查询查询速度

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/player/mapper/PlayerMapper.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/player/mapper/PlayerMapper.java
@@ -9,6 +9,7 @@ import org.jeecg.modules.player.entity.PlayerDTO;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author jeecg-boot
@@ -31,5 +32,16 @@ public interface PlayerMapper extends BaseMapper<Player> {
 	                                         @Param("playerId") Long playerId,
 	                                         @Param("logTable") String logTable);
 
+	/**
+	 * 查询所有用户注册信息（唯一不重复）
+	 * @return
+	 */
+	@Select("select player_id, create_time from game_register_info where server_id = #{serverId} order by create_time desc")
+	List<Map> selectAllPlayerInfo(@Param("serverId") Integer serverId);
+	/**
+	 * 查询所有用户登录信息
+	 */
+	@Select("select player_id, create_time from xiuzhen_log_test.`log_account` where type = 2 and server_id = #{serverId} order by create_time desc")
+	List<Map> selectAllLoginInfo(@Param("serverId") Integer serverId);
 
 }
