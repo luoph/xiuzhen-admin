7b116a833281020a25723feb96df67daa7b75855
luopeihuan <luopeihuan@gzyouai.com>
2020-12-28 15:35:16 +0800

    增加gm地址判断，过滤localhost调用

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/impl/GameServerServiceImpl.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/impl/GameServerServiceImpl.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.google.common.primitives.Ints;
+import org.apache.commons.lang3.StringUtils;
 import org.jeecg.common.okhttp.OkHttpHelper;
 import org.jeecg.modules.game.entity.GameServer;
 import org.jeecg.modules.game.mapper.GameServerMapper;
@@ -30,6 +31,11 @@ public class GameServerServiceImpl extends ServiceImpl<GameServerMapper, GameSer
         Map<Integer, Response> responseMap = new HashMap<>(serverIds.size());
         for (Integer serverId : serverIds) {
             GameServer gameServer = getById(serverId);
+            if (StringUtils.contains(gameServer.getGmUrl(), "localhost")
+                    || StringUtils.contains(gameServer.getGmUrl(), "127.0.0.1")) {
+                continue;
+            }
+
             try {
                 Response response = JSON.parseObject(OkHttpHelper.get(gameServer.getGmUrl() + path), Response.class);
                 responseMap.put(serverId, response);
@@ -50,6 +56,11 @@ public class GameServerServiceImpl extends ServiceImpl<GameServerMapper, GameSer
         Map<String, Response> responseMap = new HashMap<>(serverIds.size());
         for (String serverId : serverIds) {
             GameServer gameServer = getById(serverId);
+            if (StringUtils.contains(gameServer.getGmUrl(), "localhost")
+                    || StringUtils.contains(gameServer.getGmUrl(), "127.0.0.1")) {
+                continue;
+            }
+
             try {
                 Response response = JSON.parseObject(OkHttpHelper.get(gameServer.getGmUrl() + path, params), Response.class);
                 responseMap.put(serverId, response);
@@ -70,6 +81,11 @@ public class GameServerServiceImpl extends ServiceImpl<GameServerMapper, GameSer
         Map<Integer, Response> responseMap = new HashMap<>(serverIds.size());
         for (Integer serverId : serverIds) {
             GameServer gameServer = getById(serverId);
+            if (StringUtils.contains(gameServer.getGmUrl(), "localhost")
+                    || StringUtils.contains(gameServer.getGmUrl(), "127.0.0.1")) {
+                continue;
+            }
+
             try {
                 Response response = JSON.parseObject(OkHttpHelper.post(gameServer.getGmUrl() + path, data), Response.class);
                 responseMap.put(serverId, response);
