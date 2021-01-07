8c5bc52f55c90794a3de18d24b45137d87b378b4
huli <yw15397919336@163.com>
2021-01-07 12:23:10 +0800

    新增、分档、首付、免费留存动态表头和动态天数查询修改，排序问题优化

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/IRemainStatisticsService.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/IRemainStatisticsService.java
@@ -1,5 +1,6 @@
 package org.jeecg.modules.game.service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.jeecg.modules.game.entity.GameRemainStatistisc;
@@ -9,14 +10,20 @@ import java.util.List;
 
 public interface IRemainStatisticsService extends IService<GameRemainStatistisc> {
     /**
-     * 查询留存数---直接操作数据库方式(缺点，时间跨度大会很慢)
+     * 查询新增留存数---直接操作数据库方式(缺点，时间跨度大会很慢)(废弃)
      */
     List<GameRemainStatistisc> queryRemainStatistiscOfNewUserlListA(String rangeDateBegin, String rangeDateEnd, String tableName, int serverId, String channelName);
 
     /**
-     * 查询留存数---减少操作数据库方式
+     * 查询新增留存数---减少操作数据库方式
      */
     List<GameRemainStatistisc> queryRemainStatistiscOfNewUserlListB(String rangeDateBegin, String rangeDateEnd, String tableName, int serverId, String channelName);
+
+    /**
+     * 查询新增留存数---返回List<JSONObject>
+     */
+    List<JSONObject> queryRemainStatistiscOfNewUserlListJsonObjectList(String rangeDateBegin, String rangeDateEnd, String tableName, int serverId, String channelName, String daysRange);
+
 
     /**
      * 查询首付留存数
@@ -24,7 +31,13 @@ public interface IRemainStatisticsService extends IService<GameRemainStatistisc>
     List<GameRemainStatistisc> queryRemainStatistiscOfDownPaymentList(String rangeDateBegin, String rangeDateEnd, String tableName, int serverId, String channelName);
 
     /**
-     * 免费留存
+     * 查询首付留存数---返回List<JSONObject>
+     */
+    List<JSONObject> queryRemainStatistiscOfDownPaymentListJsonObjectList(String rangeDateBegin, String rangeDateEnd, String tableName, int serverId, String channelName, String daysRange);
+
+
+    /**
+     * 免费留存（废弃）
      */
     List<GameRemainStatistisc> queryRemainStatistiscOfFreeList(String rangeDateBegin, String rangeDateEnd, String tableName, int serverId, String channelName);
 
@@ -34,11 +47,22 @@ public interface IRemainStatisticsService extends IService<GameRemainStatistisc>
     List<GameRemainStatistisc> queryRemainStatistiscOfFreeListB(String rangeDateBegin, String rangeDateEnd, String tableName, int serverId, String channelName) throws IllegalAccessException, InvocationTargetException, JsonProcessingException, Exception;
 
     /**
-     * 分档位留存
+     * 免费留存---返回List<JSONObject>
+     */
+    List<JSONObject> queryRemainStatistiscOfFreeListBJsonObjectList(String rangeDateBegin, String rangeDateEnd, String tableName, int serverId, String channelName, String daysRange) throws IllegalAccessException, InvocationTargetException, JsonProcessingException, Exception;
+
+
+    /**
+     * 分档位留存（废弃）
      */
     List<GameRemainStatistisc> queryRemainStatistiscOfGradeList(String rangeDateBegin, String rangeDateEnd, String tableName, int serverId, String channelName);
     /**
      * 分档位留存
      */
     List<GameRemainStatistisc> queryRemainStatistiscOfGradeListB(String rangeDateBegin, String rangeDateEnd, String tableName, int serverId, String channelName);
+
+    /**
+     * 分档位留存
+     */
+    List<JSONObject> queryRemainStatistiscOfGradeListBJsonObjectList(String rangeDateBegin, String rangeDateEnd, String tableName, int serverId, String channelName, String daysRange);
 }
