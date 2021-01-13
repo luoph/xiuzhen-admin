7f3e2da6a92ff6c06fca36fcce6023c662d3b9a8
luopeihuan <luopeihuan@gzyouai.com>
2021-01-13 17:06:55 +0800

    新增调查问卷配置

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/entity/GameQuestionnaire.java
@@ -0,0 +1,80 @@
+package org.jeecg.modules.game.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import org.jeecg.common.constant.TimeConstant;
+import org.jeecg.common.system.base.entity.BaseEntity;
+import org.jeecgframework.poi.excel.annotation.Excel;
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ * @author jeecg-boot
+ * @version V1.0
+ * @description 问卷调查
+ * @date 2021-01-13
+ */
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+@TableName("game_questionnaire")
+public class GameQuestionnaire extends BaseEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id
+     */
+    @TableId(type = IdType.AUTO)
+    private java.lang.Integer id;
+
+    /**
+     * 服务器id，使用,分割
+     */
+    @Excel(name = "服务器id", width = 15)
+    private java.lang.String serverIds;
+
+    /**
+     * 已刷新的服务器id，使用,分割
+     */
+    @Excel(name = "已刷新的服务器id", width = 15)
+    private java.lang.String lastServerIds;
+
+    /**
+     * 问卷调查地址
+     */
+    @Excel(name = "问卷调查地址", width = 15)
+    private java.lang.String url;
+
+    /**
+     * 状态: 0.关闭, 1.开启
+     */
+    @Excel(name = "状态", width = 15)
+    private java.lang.Integer status;
+
+    /**
+     * 开始时间
+     */
+    @Excel(name = "开始时间", width = 15, format = TimeConstant.DEFAULT_TIME_FORMAT)
+    @JsonFormat(timezone = TimeConstant.DEFAULT_TIMEZONE, pattern = TimeConstant.DEFAULT_TIME_FORMAT)
+    @DateTimeFormat(pattern = TimeConstant.DEFAULT_TIME_FORMAT)
+    private java.util.Date startTime;
+
+    /**
+     * 结束时间
+     */
+    @Excel(name = "结束时间", width = 15, format = TimeConstant.DEFAULT_TIME_FORMAT)
+    @JsonFormat(timezone = TimeConstant.DEFAULT_TIMEZONE, pattern = TimeConstant.DEFAULT_TIME_FORMAT)
+    @DateTimeFormat(pattern = TimeConstant.DEFAULT_TIME_FORMAT)
+    private java.util.Date endTime;
+
+    /**
+     * 备注
+     */
+    @Excel(name = "备注", width = 15)
+    private java.lang.String remark;
+}
--- jeecg-vue/src/views/game/GameQuestionnaireList.vue
+++ jeecg-vue/src/views/game/GameQuestionnaireList.vue
@@ -5,35 +5,32 @@
             <a-form layout="inline" @keyup.enter.native="searchQuery">
                 <a-row :gutter="24">
                     <a-col :md="6" :sm="8">
-                        <a-form-item label="活动名称">
-                            <j-input placeholder="请输入活动名称模糊查询" v-model="queryParam.name"></j-input>
+                        <a-form-item label="问卷调查地址">
+                            <j-input placeholder="请输入问卷调查地址" v-model="queryParam.url"></j-input>
                         </a-form-item>
                     </a-col>
                     <a-col :md="6" :sm="8">
-                        <a-form-item label="活动状态">
-                            <a-select placeholder="请选择活动状态" v-model="queryParam.status" initialValue="1">
-                                <a-select-option :value="0">无效</a-select-option>
-                                <a-select-option :value="1">有效</a-select-option>
+                        <a-form-item label="状态">
+                            <a-select placeholder="请选择状态" v-model="queryParam.status" initialValue="0">
+                                <a-select-option :value="0">关闭</a-select-option>
+                                <a-select-option :value="1">开启</a-select-option>
                             </a-select>
                         </a-form-item>
                     </a-col>
                     <template v-if="toggleSearchStatus">
-                        <a-col :md="6" :sm="8">
-                            <a-form-item label="自动开启">
-                                <a-select placeholder="请选择自动开启状态" v-model="queryParam.autoOpen" initialValue="0">
-                                    <a-select-option :value="0">关闭</a-select-option>
-                                    <a-select-option :value="1">开启</a-select-option>
-                                </a-select>
+                        <a-col :md="8" :sm="16">
+                            <a-form-item label="活动开始时间">
+                                <a-range-picker v-model="queryParam.startTimeRange" format="YYYY-MM-DD" :placeholder="['开始时间', '结束时间']" @change="onStartTimeChange" />
+                            </a-form-item>
+                        </a-col>
+                        <a-col :md="8" :sm="16">
+                            <a-form-item label="活动结束时间">
+                                <a-range-picker v-model="queryParam.endTimeRange" format="YYYY-MM-DD" :placeholder="['开始时间', '结束时间']" @change="onEndTimeChange" />
                             </a-form-item>
                         </a-col>
                         <a-col :md="6" :sm="8">
-                            <a-form-item label="活动备注">
-                                <j-input placeholder="请输入活动备注模糊查询" v-model="queryParam.remark"></j-input>
-                            </a-form-item>
-                        </a-col>
-                        <a-col :md="12" :sm="16">
-                            <a-form-item label="创建时间">
-                                <a-range-picker v-model="queryParam.createTimeRange" format="YYYY-MM-DD" :placeholder="['开始时间', '结束时间']" @change="onCreateTimeChange" />
+                            <a-form-item label="备注">
+                                <a-input placeholder="请输入备注" v-model="queryParam.remark"></a-input>
                             </a-form-item>
                         </a-col>
                     </template>
@@ -54,7 +51,7 @@
         <!-- 操作按钮区域 -->
         <div class="table-operator">
             <a-button type="primary" icon="plus" @click="handleAdd">新增</a-button>
-            <a-button type="primary" icon="download" @click="handleExportXls('开服活动(1级)')">导出</a-button>
+            <a-button type="primary" icon="download" @click="handleExportXls('问卷调查')">导出</a-button>
             <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
                 <a-button type="primary" icon="import">导入</a-button>
             </a-upload>
@@ -91,23 +88,16 @@
                 </template>
                 <template slot="imgSlot" slot-scope="text">
                     <span v-if="!text" style="font-size: 12px;font-style: italic;">无此图片</span>
-                    <img v-else :src="getImgView(text)" alt="图片不存在" class="image" />
+                    <img v-else :src="getImgView(text)" height="25px" alt="图片不存在" style="max-width:80px;font-size: 12px;font-style: italic;" />
                 </template>
                 <template slot="fileSlot" slot-scope="text">
                     <span v-if="!text" style="font-size: 12px;font-style: italic;">无此文件</span>
                     <a-button v-else :ghost="true" type="primary" icon="download" size="small" @click="uploadFile(text)"> 下载 </a-button>
                 </template>
-                <span slot="serverIdTags" slot-scope="text, record">
-                    <a-tag v-if="!text" color="red">未设置</a-tag>
-                    <a-tag v-else v-for="tag in text.split(',')" :key="tag" color="blue">{{ tag }}</a-tag>
-                </span>
 
                 <span slot="action" slot-scope="text, record">
-                    <a @click="handleEdit(record)">活动信息</a>
+                    <a @click="handleEdit(record)">编辑</a>
                     <a-divider type="vertical" />
-                    <a @click="handleDuplicate(record)">复制</a>
-                    <a-divider type="vertical" />
-                    <!-- <a @click="handleTabList(record)">页签配置</a> -->
                     <a @click="handleSync(record)">同步到区服</a>
                     <a-divider type="vertical" />
                     <a-dropdown>
@@ -124,31 +114,27 @@
             </a-table>
         </div>
 
-        <open-service-campaign-modal ref="modalForm" @ok="modalFormOk"></open-service-campaign-modal>
-        <!-- <open-service-campaign-server-list ref="serverListModal" @ok="modalFormOk"></open-service-campaign-server-list> -->
-        <!-- <open-service-campaign-tab-list ref="tabListModal"></open-service-campaign-tab-list> -->
+        <game-questionnaire-modal ref="modalForm" @ok="modalFormOk"></game-questionnaire-modal>
     </a-card>
 </template>
 
 <script>
 import { JeecgListMixin } from "@/mixins/JeecgListMixin";
-import { getAction } from "@/api/manage";
-import { filterObj } from "@/utils/util";
-import OpenServiceCampaignModal from "./modules/OpenServiceCampaignModal";
+import GameQuestionnaireModal from "./modules/GameQuestionnaireModal";
 import JDate from "@/components/jeecg/JDate.vue";
-import JInput from "@/components/jeecg/JInput";
+import { filterObj } from "@/utils/util";
+import { getAction } from "@/api/manage";
 
 export default {
-    name: "OpenServiceCampaignList",
+    name: "GameQuestionnaireList",
     mixins: [JeecgListMixin],
     components: {
         JDate,
-        JInput,
-        OpenServiceCampaignModal
+        GameQuestionnaireModal
     },
     data() {
         return {
-            description: "开服活动(1级)管理页面",
+            description: "问卷调查管理页面",
             confirmLoading: false,
             // 表头
             columns: [
@@ -163,45 +149,19 @@ export default {
                     }
                 },
                 {
-                    title: "id",
-                    align: "center",
-                    dataIndex: "id"
-                },
-                {
-                    title: "活动名称",
-                    align: "center",
-                    dataIndex: "name"
-                },
-                {
                     title: "服务器id",
                     align: "center",
-                    dataIndex: "serverIds",
-                    scopedSlots: { customRender: "serverIdTags" }
+                    dataIndex: "serverIds"
                 },
                 {
-                    title: "活动图标",
+                    title: "问卷调查地址",
                     align: "center",
-                    dataIndex: "icon",
-                    scopedSlots: { customRender: "imgSlot" }
+                    dataIndex: "url"
                 },
                 {
-                    title: "活动状态",
+                    title: "状态",
                     align: "center",
                     dataIndex: "status",
-                    customRender: value => {
-                        let re = "--";
-                        if (value === 0) {
-                            re = "无效";
-                        } else if (value === 1) {
-                            re = "有效";
-                        }
-                        return re;
-                    }
-                },
-                {
-                    title: "自动开启",
-                    align: "center",
-                    dataIndex: "autoOpen",
                     customRender: value => {
                         let text = "--";
                         if (value === 0) {
@@ -213,7 +173,17 @@ export default {
                     }
                 },
                 {
-                    title: "活动备注",
+                    title: "开始时间",
+                    align: "center",
+                    dataIndex: "startTime"
+                },
+                {
+                    title: "结束时间",
+                    align: "center",
+                    dataIndex: "endTime"
+                },
+                {
+                    title: "备注",
                     align: "center",
                     dataIndex: "remark"
                 },
@@ -222,11 +192,6 @@ export default {
                     align: "center",
                     dataIndex: "createTime"
                 },
-                // {
-                //     title: "更新时间",
-                //     align: "center",
-                //     dataIndex: "updateTime"
-                // },
                 {
                     title: "操作",
                     dataIndex: "action",
@@ -235,13 +200,13 @@ export default {
                 }
             ],
             url: {
-                list: "game/openServiceCampaign/list",
-                sync: "game/openServiceCampaign/sync",
-                delete: "game/openServiceCampaign/delete",
-                duplicate: "game/openServiceCampaign/duplicate",
-                deleteBatch: "game/openServiceCampaign/deleteBatch",
-                exportXlsUrl: "game/openServiceCampaign/exportXls",
-                importExcelUrl: "game/openServiceCampaign/importExcel"
+                list: "game/questionnaire/list",
+                sync: "game/questionnaire/sync",
+                delete: "game/questionnaire/delete",
+                duplicate: "game/questionnaire/duplicate",
+                deleteBatch: "game/questionnaire/deleteBatch",
+                exportXlsUrl: "game/questionnaire/exportXls",
+                importExcelUrl: "game/questionnaire/importExcel"
             },
             dictOptions: {}
         };
@@ -259,18 +224,19 @@ export default {
             param.pageNo = this.ipagination.current;
             param.pageSize = this.ipagination.pageSize;
             // 范围参数不传递后台
-            delete param.createTimeRange;
+            delete param.startTimeRange;
+            delete param.endTimeRange;
             return filterObj(param);
         },
-        onCreateTimeChange: function(value, dateString) {
+        onStartTimeChange: function(value, dateString) {
             console.log(dateString[0], dateString[1]);
-            this.queryParam.createTime_begin = dateString[0];
-            this.queryParam.createTime_end = dateString[1];
+            this.queryParam.startTime_begin = dateString[0];
+            this.queryParam.startTime_end = dateString[1];
         },
-        handleEdit: function(record) {
-            this.$refs.modalForm.edit(record);
-            this.$refs.modalForm.title = "活动信息";
-            this.$refs.modalForm.disableSubmit = false;
+        onEndTimeChange: function(value, dateString) {
+            console.log(dateString[0], dateString[1]);
+            this.queryParam.endTime_begin = dateString[0];
+            this.queryParam.endTime_end = dateString[1];
         },
         handleDuplicate: function(record) {
             const that = this;
@@ -301,21 +267,6 @@ export default {
                 .finally(() => {
                     that.confirmLoading = false;
                 });
-        },
-        handleServerList: function(record) {
-            this.$refs.serverListModal.edit(record);
-            this.$refs.serverListModal.title = "活动信息";
-        },
-        handleTabList: function(record) {
-            this.$refs.tabListModal.edit(record);
-            this.$refs.tabListModal.title = "页签配置";
-            this.$refs.tabListModal.disableSubmit = false;
-        },
-        getImgView(text) {
-            if (text && text.indexOf(",") > 0) {
-                text = text.substring(0, text.indexOf(","));
-            }
-            return `${window._CONFIG["domainURL"]}/${text}`;
         }
     }
 };
@@ -323,10 +274,4 @@ export default {
 
 <style scoped>
 @import "~@assets/less/common.less";
-
-.image {
-    width: 100%;
-    height: 100px;
-    object-fit: scale-down;
-}
 </style>
