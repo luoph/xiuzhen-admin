81e0a8b256645c674303e625b3250efa7d3d89b9
huli <yw15397919336@163.com>
2021-01-14 18:19:08 +0800

    代码规范3

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/impl/GamePlayMethodsTakePartInServiceImpl.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/impl/GamePlayMethodsTakePartInServiceImpl.java
@@ -4,10 +4,13 @@ import cn.hutool.core.date.DatePattern;
 import cn.youai.xiuzhen.utils.BigDecimalUtil;
 import cn.youai.xiuzhen.utils.DateUtils;
 import org.jeecg.modules.game.entity.GamePlayMethodsTakePartInVO;
+import org.jeecg.modules.game.entity.LogAcount;
+import org.jeecg.modules.game.entity.LogPlayer;
 import org.jeecg.modules.game.mapper.PlayMethodsTakePartInMapper;
 import org.jeecg.modules.game.service.IGamePlayMethodsTakePartInService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import sun.rmi.runtime.Log;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
@@ -22,39 +25,41 @@ import java.util.stream.Collectors;
 
 @Service
 public class GamePlayMethodsTakePartInServiceImpl implements IGamePlayMethodsTakePartInService {
+
     @Resource
     PlayMethodsTakePartInMapper playMethodsTakePartInMapper;
     @Value("${app.log.db.table_log_player}")
     String logPlayerTable;
     @Value("${app.log.db.table}")
     String logAccountTable;
+
     @Override
-    public List<GamePlayMethodsTakePartInVO> playMethodsTakePartList( int fullTimes, int grade, String playMethodsType, Date createDateBegin, Date createDateEnd, int serverId) {
+    public List<GamePlayMethodsTakePartInVO> playMethodsTakePartList(int fullTimes, int grade, String playMethodsType, Date createDateBegin, Date createDateEnd, int serverId) {
 
         List<GamePlayMethodsTakePartInVO> gamePlayMethodsTakePartInVOList = new ArrayList<>();
         // 查询时间范围内某玩法类型的玩家日志
-        List<Map<String, Object>> playerLogList = playMethodsTakePartInMapper.conditionSelectPlayerLog(playMethodsType, createDateBegin, createDateEnd, logPlayerTable, serverId);
+        List<LogPlayer> playerLogList = playMethodsTakePartInMapper.conditionSelectPlayerLog(playMethodsType, createDateBegin, createDateEnd, logPlayerTable, serverId);
         // 玩家日志日志以时间分组
-        Map<String, List<Map<String, Object>>> playerLogListMapCreateDate = playerLogList.stream().collect(Collectors.groupingBy(map -> map.get("create_date").toString()));
+        Map<Date, List<LogPlayer>> playerLogListMapCreateDate = playerLogList.stream().collect(Collectors.groupingBy(logPlayer -> logPlayer.getCreateDate()));
         //查询到达某等级用户的登录日志
-        List<Map<String, Object>> accountLogList = playMethodsTakePartInMapper.selectPlayLoginInfo(grade, logAccountTable, serverId);
+        List<LogAcount> accountLogList = playMethodsTakePartInMapper.selectPlayLoginInfo(grade, logAccountTable, serverId);
         //登录日志以时间分组
-        Map<String, List<Map<String, Object>>> accountLogListCreateDate = accountLogList.stream().collect(Collectors.groupingBy(map -> map.get("create_date").toString()));
+        Map<Date, List<LogAcount>> accountLogListCreateDate = accountLogList.stream().collect(Collectors.groupingBy(logAcount -> logAcount.getCreateDate()));
 
-        for (String s : playerLogListMapCreateDate.keySet()) {
+        for (Date s : playerLogListMapCreateDate.keySet()) {
             //时间内玩家日志以用户id分组
-            Map<String ,List<Map<String, Object>>> playerLogListMapCreateDatePlayerId =  playerLogListMapCreateDate.get(s).stream().collect(Collectors.groupingBy(map -> map.get("player_id").toString()));
+            Map<Integer, List<LogPlayer>> playerLogListMapCreateDatePlayerId = playerLogListMapCreateDate.get(s).stream().collect(Collectors.groupingBy(logPlayer -> logPlayer.getPlayerId()));
 
             //时间内登录日志以用户id分组
-            Map<String, List<Map<String, Object>>> accountLogListCreateDatePlayerId;
+            Map<Integer, List<LogAcount>> accountLogListCreateDatePlayerId;
             if (null == accountLogListCreateDate.get(s)) {
                 continue;
             } else {
-                accountLogListCreateDatePlayerId = accountLogListCreateDate.get(s).stream().collect(Collectors.groupingBy(map -> map.get("player_id").toString()));
+                accountLogListCreateDatePlayerId = accountLogListCreateDate.get(s).stream().collect(Collectors.groupingBy(account -> account.getPlayerId()));
             }
-            GamePlayMethodsTakePartInVO  gamePlayMethodsTakePartInVO = new GamePlayMethodsTakePartInVO();
+            GamePlayMethodsTakePartInVO gamePlayMethodsTakePartInVO = new GamePlayMethodsTakePartInVO();
             //设置日期
-            gamePlayMethodsTakePartInVO.setDate(DateUtils.parseDate(s));
+            gamePlayMethodsTakePartInVO.setDate(s);
             //设置参与人数
             int takePlayerNum = playerLogListMapCreateDatePlayerId.size();
             gamePlayMethodsTakePartInVO.setTakePlayInPlayerNum(takePlayerNum);
@@ -62,59 +67,59 @@ public class GamePlayMethodsTakePartInServiceImpl implements IGamePlayMethodsTak
             int playerNum = accountLogListCreateDatePlayerId.size();
             gamePlayMethodsTakePartInVO.setPlayerNum(playerNum);
             //设置参与率
-            gamePlayMethodsTakePartInVO.setTakePlayInRate(BigDecimalUtil.divide(takePlayerNum * 100, playerNum,2));
+            gamePlayMethodsTakePartInVO.setTakePlayInRate(BigDecimalUtil.divide(takePlayerNum * 100, playerNum, 2));
             //设置满参与人数
-                //100以上的类型记录的是次数
-            if ( Integer.parseInt(playMethodsType) >= 100 ) {
+            //100以上的类型记录的是次数
+            if (Integer.parseInt(playMethodsType) >= 100) {
                 //时间内玩家日志以用户id分组 且 过滤掉没有达到满次的数据
-                Map<String ,List<Map<String, Object>>> playerLogListMapCreateDatePlayerIdFullTimes = new HashMap<>(16);
-                for (String s1 : playerLogListMapCreateDatePlayerId.keySet()) {
-                    if ( playerLogListMapCreateDatePlayerId.get(s1).size() >= fullTimes)  {
-                        playerLogListMapCreateDatePlayerIdFullTimes.put(s1, playerLogListMapCreateDatePlayerId.get(s1));
+                Map<Integer, List<LogPlayer>> playerLogListMapCreateDatePlayerIdFullTimes = new HashMap<>(16);
+                for (Integer mapKey : playerLogListMapCreateDatePlayerId.keySet()) {
+                    if (playerLogListMapCreateDatePlayerId.get(mapKey).size() >= fullTimes) {
+                        playerLogListMapCreateDatePlayerIdFullTimes.put(mapKey, playerLogListMapCreateDatePlayerId.get(mapKey));
                     }
                 }
                 gamePlayMethodsTakePartInVO.setAllTakePlayInPlayerNum(playerLogListMapCreateDatePlayerIdFullTimes.size());
                 //100以下记录的是最大值
-            }else{
-                List<Map<String, Object>> playerOneDayPlayLogFullTimes = new ArrayList<>();
-                for (String s1 : playerLogListMapCreateDatePlayerId.keySet()) {
-                    List<Map<String, Object>> playerOneDayPlayLog = playerLogListMapCreateDatePlayerId.get(s1);
+            } else {
+                List<LogPlayer> playerOneDayPlayLogFullTimes = new ArrayList<>();
+                for (Integer mapKey : playerLogListMapCreateDatePlayerId.keySet()) {
+                    List<LogPlayer> playerOneDayPlayLog = playerLogListMapCreateDatePlayerId.get(mapKey);
                     //过滤出最大值 达到满次的数据
-                    playerOneDayPlayLogFullTimes = playerOneDayPlayLog.stream().filter(map -> Integer.parseInt(map.get("value").toString()) >= fullTimes).collect(Collectors.toList());
+                    playerOneDayPlayLogFullTimes = playerOneDayPlayLog.stream().filter(logPlayer -> logPlayer.getValue() >= fullTimes).collect(Collectors.toList());
                 }
                 gamePlayMethodsTakePartInVO.setAllTakePlayInPlayerNum(playerOneDayPlayLogFullTimes.size());
             }
             //设置满参率
-            gamePlayMethodsTakePartInVO.setAllTakePlayInRate(BigDecimalUtil.divide(gamePlayMethodsTakePartInVO.getAllTakePlayInPlayerNum() * 100, playerNum,2));
+            gamePlayMethodsTakePartInVO.setAllTakePlayInRate(BigDecimalUtil.divide(gamePlayMethodsTakePartInVO.getAllTakePlayInPlayerNum() * 100, playerNum, 2));
             //设置回头率（昨天有参与该玩法的玩家中，今天参与的人数/这些玩家今天有登录的人数）
-            Date yesterdayDate = DateUtils.addDays(DateUtils.parseDate(s), -1);
-              //昨天参与该玩法的玩家
-            List<Map<String, Object>> yesterdayDateData = playerLogListMapCreateDate.get(DateUtils.formatDate(yesterdayDate, DatePattern.NORM_DATE_PATTERN));
+            Date yesterdayDate = DateUtils.addDays(s, -1);
+            //昨天参与该玩法的玩家
+            List<LogPlayer> yesterdayDateData = playerLogListMapCreateDate.get(yesterdayDate);
             if (null == yesterdayDateData) {
                 gamePlayMethodsTakePartInVO.setSecondGlanceRate(new BigDecimal(0));
                 gamePlayMethodsTakePartInVOList.add(gamePlayMethodsTakePartInVO);
                 continue;
             }
-              //昨天参与该玩法的玩家 以player_id分组
-            Map<String ,List<Map<String, Object>>> yesterdayDateDataMapPlayerId =  yesterdayDateData.stream().collect(Collectors.groupingBy(map -> map.get("player_id").toString()));
-              //昨天参与该玩法的玩家 今天参与的人数
+            //昨天参与该玩法的玩家 以player_id分组
+            Map<Integer, List<LogPlayer>> yesterdayDateDataMapPlayerId = yesterdayDateData.stream().collect(Collectors.groupingBy(logplayer -> logplayer.getPlayerId()));
+            //昨天参与该玩法的玩家 今天参与的人数
             int yesterdayHasPlayInTodaySum = 0;
-            for (String s1 : yesterdayDateDataMapPlayerId.keySet()) {
-                if (null != playerLogListMapCreateDatePlayerId.get(s1)) {
-                    yesterdayHasPlayInTodaySum ++;
+            for (Integer mapkey : yesterdayDateDataMapPlayerId.keySet()) {
+                if (null != playerLogListMapCreateDatePlayerId.get(mapkey)) {
+                    yesterdayHasPlayInTodaySum++;
                 }
             }
-              //昨天参与该玩法的玩家 今天有登录的人数
+            //昨天参与该玩法的玩家 今天有登录的人数
             int yesterdayHasLoginInTodaySum = 0;
-            for (String s1 : yesterdayDateDataMapPlayerId.keySet()) {
-                if (null != accountLogListCreateDatePlayerId.get(s1)) {
-                    yesterdayHasLoginInTodaySum ++;
+            for (Integer mapkey : yesterdayDateDataMapPlayerId.keySet()) {
+                if (null != accountLogListCreateDatePlayerId.get(mapkey)) {
+                    yesterdayHasLoginInTodaySum++;
                 }
             }
             if (0 == yesterdayHasLoginInTodaySum) {
                 gamePlayMethodsTakePartInVO.setSecondGlanceRate(new BigDecimal(0));
             } else {
-                gamePlayMethodsTakePartInVO.setSecondGlanceRate(BigDecimalUtil.divide(yesterdayHasPlayInTodaySum * 100, yesterdayHasLoginInTodaySum,2));
+                gamePlayMethodsTakePartInVO.setSecondGlanceRate(BigDecimalUtil.divide(yesterdayHasPlayInTodaySum * 100, yesterdayHasLoginInTodaySum, 2));
             }
             gamePlayMethodsTakePartInVOList.add(gamePlayMethodsTakePartInVO);
         }
