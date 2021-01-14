818ea996a387de33e92126c51898dc7ace5920a7
ocean <linzhengqiang@gzyouai.com>
2021-01-14 18:52:05 +0800

    整理代码，移除重复

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/controller/GameCommonModuleController.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/controller/GameCommonModuleController.java
@@ -9,100 +9,57 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 前端公共组件后端支持
+ *
  * @author huli
  */
 @RestController
 @RequestMapping("game/commonModule")
 public class GameCommonModuleController {
+
     /**
      * 玩法类型前端展示选择列表
      */
+    // TODO url命名错误(小驼峰)
     @RequestMapping("/playMethodsTypeShowlist")
     public JSONObject list() {
         JSONObject response = new JSONObject();
         JSONObject result = new JSONObject();
         JSONArray record = new JSONArray();
-        //福地夺宝
-        JSONObject arenaBattle = new JSONObject();
-        arenaBattle.put("type", PlayerLogType.ARENA_BATTLE.getType() + "");
-        arenaBattle.put("name", PlayerLogType.ARENA_BATTLE.getName());
-        arenaBattle.put("grade", "38");
-        arenaBattle.put("fullTime", "10");
-        record.add(arenaBattle);
-        //上古遗迹
-        JSONObject travelHill = new JSONObject();
-        travelHill.put("type", PlayerLogType.TRAVEL_HILL.getType() + "");
-        travelHill.put("name", PlayerLogType.TRAVEL_HILL.getName());
-        travelHill.put("grade", "40");
-        travelHill.put("fullTime", "3");
-        record.add(travelHill);
-        //神魔战场
-        JSONObject ghostWar = new JSONObject();
-        ghostWar.put("type", PlayerLogType.GHOST_WAR.getType() + "");
-        ghostWar.put("name", PlayerLogType.GHOST_WAR.getName());
-        ghostWar.put("grade", "1");
-        ghostWar.put("fullTime", "38");
-        record.add(ghostWar);
-        //斗灵道场
-        //北冥魔海
-        JSONObject mainStoryBoss = new JSONObject();
-        mainStoryBoss.put("type", PlayerLogType.MAIN_STORY_BOSS.getType() + "");
-        mainStoryBoss.put("name", PlayerLogType.MAIN_STORY_BOSS.getName());
-        mainStoryBoss.put("grade", "34");
-        mainStoryBoss.put("fullTime", "8");
-        record.add(mainStoryBoss);
-        //不死魔巢
-        JSONObject petBoss = new JSONObject();
-        petBoss.put("type", PlayerLogType.PET_BOSS.getType() + "");
-        petBoss.put("name", PlayerLogType.PET_BOSS.getName());
-        petBoss.put("grade", "37");
-        petBoss.put("fullTime", "6");
-        record.add(petBoss);
-        //蛇陵魔窟
-        JSONObject spiritualWorldBoss = new JSONObject();
-        spiritualWorldBoss.put("type", PlayerLogType.SPIRITUAL_WORLD_BOSS.getType() + "");
-        spiritualWorldBoss.put("name", PlayerLogType.SPIRITUAL_WORLD_BOSS.getName());
-        spiritualWorldBoss.put("grade", "45");
-        spiritualWorldBoss.put("fullTime", "3");
-        record.add(spiritualWorldBoss);
-        //魔王入侵
-        JSONObject worldBoss = new JSONObject();
-        worldBoss.put("type", PlayerLogType.WORLD_BOSS.getType() + "");
-        worldBoss.put("name", PlayerLogType.WORLD_BOSS.getName());
-        worldBoss.put("grade", "33");
-        worldBoss.put("fullTime", "1");
-        record.add(worldBoss);
-        //仙器秘境
-        JSONObject tierMapExplore = new JSONObject();
-        tierMapExplore.put("type", PlayerLogType.TIER_MAP_EXPLORE.getType() + "");
-        tierMapExplore.put("name", PlayerLogType.TIER_MAP_EXPLORE.getName());
-        tierMapExplore.put("grade", "32");
-        tierMapExplore.put("fullTime", "3");
-        record.add(tierMapExplore);
-        //仙兽秘境
-        JSONObject mapExplore = new JSONObject();
-        mapExplore.put("type", PlayerLogType.MAP_EXPLORE.getType() + "");
-        mapExplore.put("name", PlayerLogType.MAP_EXPLORE.getName());
-        mapExplore.put("grade", "33");
-        mapExplore.put("fullTime", "10");
-        record.add(mapExplore);
-        //丹药秘境
-        JSONObject godRoad = new JSONObject();
-        godRoad.put("type", PlayerLogType.GOD_ROAD.getType() + "");
-        godRoad.put("name", PlayerLogType.GOD_ROAD.getName());
-        godRoad.put("grade", "35");
-        godRoad.put("fullTime", "3");
-        record.add(godRoad);
-        //符文秘境
-        JSONObject lingShanCheckpoint = new JSONObject();
-        lingShanCheckpoint.put("type", PlayerLogType.LINGSHAN_CHECKPOINT.getType() + "");
-        lingShanCheckpoint.put("name", PlayerLogType.LINGSHAN_CHECKPOINT.getName());
-        lingShanCheckpoint.put("grade", "39");
-        lingShanCheckpoint.put("fullTime", "1");
-        record.add(lingShanCheckpoint);
+        // 福地夺宝
+        record.add(getJsonObject(PlayerLogType.ARENA_BATTLE, 38, 10));
+        // 上古遗迹
+        record.add(getJsonObject(PlayerLogType.TRAVEL_HILL, 40, 3));
+        // 神魔战场
+        record.add(getJsonObject(PlayerLogType.GHOST_WAR, 1, 38));
+        // 斗灵道场
+        // 北冥魔海
+        record.add(getJsonObject(PlayerLogType.MAIN_STORY_BOSS, 34, 8));
+        // 不死魔巢
+        record.add(getJsonObject(PlayerLogType.PET_BOSS, 37, 6));
+        // 蛇陵魔窟
+        record.add(getJsonObject(PlayerLogType.SPIRITUAL_WORLD_BOSS, 45, 3));
+        // 魔王入侵
+        record.add(getJsonObject(PlayerLogType.WORLD_BOSS, 33, 1));
+        // 仙器秘境
+        record.add(getJsonObject(PlayerLogType.TIER_MAP_EXPLORE, 32, 3));
+        // 仙兽秘境
+        record.add(getJsonObject(PlayerLogType.MAP_EXPLORE, 33, 10));
+        // 丹药秘境
+        record.add(getJsonObject(PlayerLogType.GOD_ROAD, 35, 3));
+        // 符文秘境
+        record.add(getJsonObject(PlayerLogType.LINGSHAN_CHECKPOINT, 39, 1));
         result.put("record", record);
         response.put("result", result);
         return response;
+    }
+
+    private JSONObject getJsonObject(PlayerLogType playerLogType, int grade, int fullTime) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("type", playerLogType.getType());
+        jsonObject.put("name", playerLogType.getName());
+        jsonObject.put("grade", grade);
+        jsonObject.put("fullTime", fullTime);
+        return jsonObject;
     }
 
 }
