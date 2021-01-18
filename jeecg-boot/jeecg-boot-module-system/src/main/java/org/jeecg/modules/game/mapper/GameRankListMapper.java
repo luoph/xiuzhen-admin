136166c49e2ce863572ce6416b00d07ab8632bf2
huli <yw15397919336@163.com>
2021-01-18 11:19:40 +0800

    更改查询时间字段

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/mapper/GameRankListMapper.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/mapper/GameRankListMapper.java
@@ -43,7 +43,7 @@ public interface GameRankListMapper {
     /**
      * 查询log_player中type>=100的类型行为总数
      */
-    @Select("select player_id, count(*) as value from ${logPlayerTable} where type = #{type} and server_id = #{serverId} and create_date <= #{createTime} group by player_id having count(*) >= 1 ORDER BY count(*) desc")
+    @Select("select player_id, count(*) as value from ${logPlayerTable} where type = #{type} and server_id = #{serverId} and create_time <= #{createTime} group by player_id having count(*) >= 1 ORDER BY count(*) desc")
     List<LogPlayer> selectCount(@Param("logPlayerTable") String logPlayerTable,
                                 @Param("type") int type,
                                 @Param("createTime") Date createTime,
@@ -52,7 +52,7 @@ public interface GameRankListMapper {
     /**
      * 查询log_player中type<100的类型行为最大值
      */
-    @Select("select player_id, max(value) as value from ${logPlayerTable} where type = #{type} and server_id = #{serverId} and create_date <= #{createTime} group by player_id  ORDER BY max(value) desc")
+    @Select("select player_id, max(value) as value from ${logPlayerTable} where type = #{type} and server_id = #{serverId} and create_time <= #{createTime} group by player_id  ORDER BY max(value) desc")
     List<LogPlayer> selectSum(@Param("logPlayerTable") String logPlayerTable,
                               @Param("type") int type,
                               @Param("createTime") Date createTime,
