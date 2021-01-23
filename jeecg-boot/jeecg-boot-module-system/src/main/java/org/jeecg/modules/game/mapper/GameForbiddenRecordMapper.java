0d4b97226fd9ee26cbd8228678a106c8f6fa5e4b
luopeihuan <luopeihuan@gzyouai.com>
2021-01-23 22:07:59 +0800

    优化封禁操作，增加封禁记录

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/mapper/GameForbiddenRecordMapper.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/mapper/GameForbiddenRecordMapper.java
@@ -6,8 +6,8 @@ import org.jeecg.modules.game.entity.GameForbiddenRecord;
 /**
  * @author jeecg-boot
  * @version V1.0
- * @description game_forbidden_record
- * @date 2020-12-17
+ * @description 封禁记录
+ * @date 2021-01-23
  */
 public interface GameForbiddenRecordMapper extends BaseMapper<GameForbiddenRecord> {
 
