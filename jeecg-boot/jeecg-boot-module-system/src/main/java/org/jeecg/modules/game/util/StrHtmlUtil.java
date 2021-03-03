239f86c827d6945e580e7f4ae8d28f9d640ffdd7
buliangliang <buliangliang@gzyouai.com>
2021-03-03 17:12:00 +0800

    工具类优化

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/util/StrHtmlUtil.java
@@ -0,0 +1,26 @@
+package org.jeecg.modules.game.util;
+
+/**
+ * @ClassName StrHtmlUtil
+ * @Description html字符串工具
+ * @Author buliangliang
+ * @Version V1.0.0
+ * @Since 1.0
+ * @Date 2021/3/3 5:10 下午
+ */
+public class StrHtmlUtil {
+
+    public static String formatNoticeHtml(String input) {
+        input = input.replace("\n<p>", "<br />&nbsp;<br />");
+        input = input.replace("<br/>", "<br />");
+        input = input.replace("\r\n", "<br />");
+        input = input.replace("\n", "<br />");
+        input = input.replace("<p>", "");
+        input = input.replace("</p>", "");
+        // strong 标签无效
+        input = input.replace("<strong>", "");
+        input = input.replace("</strong>", "");
+        input = input.replace("......", "...");
+        return input;
+    }
+}
