08bc4a3374fd7ffb91b8a207fdccda9effcc6aa4
luopeihuan <luopeihuan@gzyouai.com>
2021-03-25 20:25:51 +0800

    代码整理

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/mapper/GameChannelMapper.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/mapper/GameChannelMapper.java
@@ -2,7 +2,6 @@ package org.jeecg.modules.game.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 import org.jeecg.modules.game.entity.GameChannel;
 import org.jeecg.modules.game.entity.GameServer;
 import org.jeecg.modules.game.entity.GameServerVO;
@@ -23,18 +22,13 @@ public interface GameChannelMapper extends BaseMapper<GameChannel> {
      * @param channelId 渠道id
      * @return List Of {@linkplain GameServer}
      */
-    @Select("SELECT s.`id`, s.`game_id`, s.`name`, s.`host`, UNIX_TIMESTAMP(s.`open_time`) as `open_time`," +
-            "  UNIX_TIMESTAMP(s.`online_time`) as `online_time`,  s.`login_url`, s.`status`, s.`recommend`," +
-            " s.`warning`, s.`min_version`, s.`max_version`, s.`is_maintain`" +
-            " FROM `game_server` s LEFT JOIN `game_channel_server` c ON c.`server_id` = s.`id`" +
-            " WHERE c.`channel_id` = #{channel_id}  AND c.`del_flag` = 0 ORDER BY c.`position`")
-    List<GameServerVO> getServerListByChannelId(@Param("channel_id") Integer channelId);
+    List<GameServerVO> getServerListByChannelId(@Param("channel_id") int channelId);
 
     /**
      * 超找渠道名
      *
+     * @param channelId 渠道id
      * @return 渠道名
      */
-    @Select("SELECT simple_name FROM game_channel WHERE id=#{channelId}")
-    String queryChannelNameById(@Param("channelId") Integer channelId);
+    String queryChannelNameById(@Param("channelId") int channelId);
 }
