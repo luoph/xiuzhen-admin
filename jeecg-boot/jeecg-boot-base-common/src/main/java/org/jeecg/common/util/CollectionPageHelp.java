4274264f8b91e4235180be3c95741dabb5123c51
lijunchi <2881556030@qq.com>
2021-04-23 12:30:32 +0800

    分页工具处理IndexOutOfBoundsException

--- jeecg-boot/jeecg-boot-base-common/src/main/java/org/jeecg/common/util/CollectionPageHelp.java
+++ jeecg-boot/jeecg-boot-base-common/src/main/java/org/jeecg/common/util/CollectionPageHelp.java
@@ -1,5 +1,9 @@
 package org.jeecg.common.util;
 
+import cn.hutool.core.collection.CollectionUtil;
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,9 +18,13 @@ public class CollectionPageHelp {
 
     /**
      * 多集合分页
+     * 超出结果查询范围，返回空集合
      */
-
     public static <E> List<E> pageBySubList(List<E> list, int pageSize, int currentPage) {
+        if (pageSize <= 0 || currentPage <= 0 || CollectionUtil.isEmpty(list)) {
+            return new ArrayList<>(0);
+        }
+
         int totalCount = list.size();
         int totalPage = 0;
 
@@ -29,12 +37,18 @@ public class CollectionPageHelp {
         }
 
         if (m == 0) {
-            subList = list.subList((currentPage - 1) * pageSize, pageSize * (currentPage));
+            if (totalCount < pageSize * currentPage) {
+                subList = list;
+            } else {
+                subList = list.subList((currentPage - 1) * pageSize, pageSize * currentPage);
+            }
         } else {
             if (currentPage == totalPage) {
                 subList = list.subList((currentPage - 1) * pageSize, totalCount);
-            } else {
-                subList = list.subList((currentPage - 1) * pageSize, pageSize * (currentPage));
+            } else if (currentPage > totalPage) { // 下标大于当前最大分页
+                subList = new ArrayList<>(0);
+            }else {
+                subList = list.subList((currentPage - 1) * pageSize, pageSize * currentPage);
             }
         }
         return subList;
