65699a5e01e6cbc6f959d9427fbe5735cc45b275
buliangliang <buliangliang@gzyouai.com>
2021-04-25 14:32:38 +0800

    新增字段

--- jeecg-vue/src/views/game/modules/GameRechargeGoodsModal.vue
+++ jeecg-vue/src/views/game/modules/GameRechargeGoodsModal.vue
@@ -1,28 +1,37 @@
 <template>
     <!-- <a-drawer :title="title" :width="width" placement="right" :closable="false" @close="close" :visible="visible"> -->
-    <a-modal :title="title" :width="width" :visible="visible" :confirmLoading="confirmLoading" @ok="handleOk" @cancel="handleCancel" cancelText="关闭" okText="保存">
+    <a-modal :title="title" :width="width" :visible="visible" :confirmLoading="confirmLoading" @ok="handleOk"
+             @cancel="handleCancel" cancelText="关闭" okText="保存">
         <a-spin :spinning="confirmLoading">
             <a-form :form="form">
-                    <a-form-item label="单价(创建订单实际价格)" :labelCol="labelCol" :wrapperCol="wrapperCol">
-                    <a-input-number v-decorator="['price', validatorRules.price]" placeholder="请输入单价(创建订单实际价格)" style="width: 100%" />
+                <a-form-item label="单价(创建订单实际价格)" :labelCol="labelCol" :wrapperCol="wrapperCol">
+                    <a-input-number v-decorator="['price', validatorRules.price]" placeholder="请输入单价(创建订单实际价格)"
+                                    style="width: 100%" />
                 </a-form-item>
                 <a-form-item label="折扣" :labelCol="labelCol" :wrapperCol="wrapperCol">
-                    <a-input-number v-decorator="['discount', validatorRules.discount]" placeholder="请输入折扣" style="width: 100%" />
+                    <a-input-number v-decorator="['discount', validatorRules.discount]" placeholder="请输入折扣"
+                                    style="width: 100%" />
                 </a-form-item>
                 <a-form-item label="商品名" :labelCol="labelCol" :wrapperCol="wrapperCol">
                     <a-input v-decorator="['name', validatorRules.name]" placeholder="请输入商品名"></a-input>
                 </a-form-item>
                 <a-form-item label="奖励列表" :labelCol="labelCol" :wrapperCol="wrapperCol">
-                    <a-textarea v-decorator="['items']" rows="4" placeholder="请输入奖励列表"/>
+                    <a-textarea v-decorator="['items']" rows="4" placeholder="请输入奖励列表" />
                 </a-form-item>
                 <a-form-item label="充值分类" :labelCol="labelCol" :wrapperCol="wrapperCol">
-                    <a-input-number v-decorator="['goodsType', validatorRules.goodsType]" placeholder="请输入充值分类" style="width: 100%" />
+                    <a-input-number v-decorator="['goodsType', validatorRules.goodsType]" placeholder="请输入充值分类"
+                                    style="width: 100%" />
                 </a-form-item>
                 <a-form-item label="是否记入累充（0 - 不计入 1 - 记入）" :labelCol="labelCol" :wrapperCol="wrapperCol">
-                    <a-input-number v-decorator="['amountStat', validatorRules.amountStat]" placeholder="请输入是否记入累充（0 - 不计入 1 - 记入）" style="width: 100%" />
+                    <a-input-number v-decorator="['amountStat', validatorRules.amountStat]"
+                                    placeholder="请输入是否记入累充（0 - 不计入 1 - 记入）" style="width: 100%" />
                 </a-form-item>
                 <a-form-item label="游戏币与人民币(元)的兑换比例" :labelCol="labelCol" :wrapperCol="wrapperCol">
-                    <a-input-number v-decorator="['exchange', validatorRules.exchange]" placeholder="请输入游戏币与人民币(元)的兑换比例" style="width: 100%" />
+                    <a-input-number v-decorator="['exchange', validatorRules.exchange]" placeholder="请输入游戏币与人民币(元)的兑换比例"
+                                    style="width: 100%" />
+                </a-form-item>
+                <a-form-item label="特殊标记前端用" :labelCol="labelCol" :wrapperCol="wrapperCol">
+                    <a-input-number v-decorator="['recommend']" placeholder="请输入特殊标记前端用" style="width: 100%" />
                 </a-form-item>
             </a-form>
         </a-spin>
@@ -40,8 +49,7 @@ import pick from "lodash.pick";
 
 export default {
     name: "GameRechargeGoodsModal",
-    components: {
-    },
+    components: {},
     data() {
         return {
             form: this.$form.createForm(this),
@@ -67,6 +75,7 @@ export default {
                 goodsType: { rules: [{ required: true, message: "请输入充值分类!" }] },
                 amountStat: { rules: [{ required: true, message: "请输入是否记入累充（0 - 不计入 1 - 记入）!" }] },
                 exchange: { rules: [{ required: true, message: "请输入游戏币与人民币(元)的兑换比例!" }] },
+                recommend: { rules: [{ required: true, message: "请输入特殊标记前端用!" }] }
             },
             url: {
                 add: "game/gameRechargeGoods/add",
@@ -130,12 +139,13 @@ export default {
         },
         popupCallback(row) {
             this.form.setFieldsValue(pick(row, "price", "discount", "name", "items", "goodsType", "amountStat", "addition", "exchange", "createBy"));
-        },
+        }
     }
 };
 </script>
 
-// <style lang="less" scoped></style>
+//
+<style lang="less" scoped></style>
 <style lang="less" scoped>
 /** Button按钮间距 */
 .ant-btn {
