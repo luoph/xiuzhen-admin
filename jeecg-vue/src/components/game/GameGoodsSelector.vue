525f1a4f868eaf7c312021e2a5b99d5c5356d371
buliangliang <buliangliang@gzyouai.com>
2021-04-25 15:21:08 +0800

    修改字段值

--- jeecg-vue/src/components/game/GameGoodsSelector.vue
+++ jeecg-vue/src/components/game/GameGoodsSelector.vue
@@ -1,9 +1,9 @@
 <template>
     <a-select showSearch placeholder="请选择渠道" v-model="goodsId" :initialValue="goodsId" @change="handleInput">
         <a-select-option value="">请选择</a-select-option>
-        <a-select-option v-for="item in goodsOptions" :key="item.name" :value="item.id">
+        <a-select-option v-for="item in goodsOptions" :key="item.name" :value="item.goodsId">
             <span style="display: inline-block;width: 100%" :title="item.name">
-                {{ item.id + "-" + item.name }}
+                {{ item.goodsId + "-" + item.name }}
             </span>
         </a-select-option>
     </a-select>
