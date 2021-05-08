3343acafd08c16aa8a0a5d8e05b6520272ee9f75
lijunchi <2881556030@qq.com>
2021-05-08 16:31:47 +0800

    【后台】修复导入空值时转换new BigDecimal()报错

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/converter/CustomStringToBigDecimalConverter.java
@@ -0,0 +1,46 @@
+package org.jeecg.modules.converter;
+
+import com.alibaba.excel.converters.Converter;
+import com.alibaba.excel.enums.CellDataTypeEnum;
+import com.alibaba.excel.metadata.CellData;
+import com.alibaba.excel.metadata.GlobalConfiguration;
+import com.alibaba.excel.metadata.property.ExcelContentProperty;
+import com.alibaba.excel.util.StringUtils;
+
+import java.math.BigDecimal;
+
+/**
+ * @author lijunchi
+ * @version 1.0
+ * @description: easyexcel String转BigDecimal 转换器
+ * @date 2021-5-8 16:14
+ */
+public class CustomStringToBigDecimalConverter implements Converter<BigDecimal> {
+    @Override
+    public Class supportJavaTypeKey() {
+        return String.class;
+    }
+
+    @Override
+    public CellDataTypeEnum supportExcelTypeKey() {
+        return CellDataTypeEnum.STRING;
+    }
+
+    /**
+     * 读数据时，防止传值为空，doConvertToJavaObject中new BigDecimal时会报错
+     */
+    @Override
+    public BigDecimal convertToJavaData(CellData cellData, ExcelContentProperty excelContentProperty, GlobalConfiguration globalConfiguration) throws Exception {
+        String value = cellData.getStringValue();
+        if (StringUtils.isEmpty(value)) {
+            return null;
+        }
+        return new BigDecimal(value);
+    }
+
+    @Override
+    public CellData convertToExcelData(BigDecimal bigDecimal, ExcelContentProperty excelContentProperty, GlobalConfiguration globalConfiguration) throws Exception {
+        return new CellData(bigDecimal);
+    }
+
+}
