22a6f8327da8c3cfd2eabbaa32b48a01cf7a7f9d
lijunchi <2881556030@qq.com>
2021-05-08 20:32:25 +0800

    【后台】充值统计业务代码调整

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/impl/GameStatOrderServiceImpl.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/impl/GameStatOrderServiceImpl.java
@@ -3,13 +3,17 @@ package org.jeecg.modules.game.service.impl;
 import cn.hutool.core.collection.CollectionUtil;
 import cn.youai.xiuzhen.utils.BigDecimalUtil;
 import cn.youai.xiuzhen.utils.DateUtils;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.jeecg.common.constant.CommonConstant;
 import org.jeecg.common.util.CollectionPageHelp;
+import org.jeecg.modules.game.entity.GameServer;
 import org.jeecg.modules.game.entity.GameStatOrder;
 import org.jeecg.modules.game.mapper.GameStatOrderMapper;
+import org.jeecg.modules.game.service.IGameServerService;
 import org.jeecg.modules.game.service.IGameStatOrderService;
 import org.jeecg.modules.player.service.ILogAccountService;
 import org.jeecg.modules.player.service.IPayOrderService;
@@ -17,10 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author jeecg-boot
@@ -37,6 +38,9 @@ public class GameStatOrderServiceImpl extends ServiceImpl<GameStatOrderMapper, G
     @Autowired
     ILogAccountService logAccountService;
 
+    @Autowired
+    IGameServerService gameServerService;
+
     @Override
     public IPage<GameStatOrder> queryGameStatOrderList(GameStatOrder gameStatOrder, Integer pageNo, Integer pageSize) {
         // 计算数据(按日期)
@@ -47,11 +51,15 @@ public class GameStatOrderServiceImpl extends ServiceImpl<GameStatOrderMapper, G
         }
         List<Date> datePageList = CollectionPageHelp.pageBySubList(dateList, pageSize, pageNo);
 
+        // 月份主体
         List<GameStatOrder> gameStatOrders = queryGameStatOrderList(gameStatOrder, datePageList, CommonConstant.PAY_ORDER_STAT_TYPE_DATE);
 
         if (CollectionUtil.isEmpty(gameStatOrders)) {
             return null;
         }
+
+        gameStatOrders.sort((o1, o2) -> DateUtils.daysBetween(DateUtils.parseDate(o1.getDate()), DateUtils.parseDate(o2.getDate())));
+
         int size = gameStatOrders.size();
 
         // 计算汇总数据(按月份)
@@ -79,7 +87,7 @@ public class GameStatOrderServiceImpl extends ServiceImpl<GameStatOrderMapper, G
         if (CollectionUtil.isEmpty(activePlayerByLoginDates)) {
             return null;
         }
-        // 主体
+        // body
         List<GameStatOrder> gameStatOrders = payOrderService.statOrderByDates(gameStatOrder.getServerId() != null ?
                 gameStatOrder.getServerId() : 0, dateList, type);
 
@@ -92,13 +100,17 @@ public class GameStatOrderServiceImpl extends ServiceImpl<GameStatOrderMapper, G
     }
 
     /**
-     * 统计月份
+     * 统计月份-服务器开服时间至今
      */
     private List<GameStatOrder> queryGameStatOrderListByMonth(GameStatOrder gameStatOrder) {
-        int num = DateUtils.monthsBetween(gameStatOrder.getStartDate(), gameStatOrder.getEndDate());
+        LambdaQueryWrapper<GameServer> wrapper = Wrappers.lambdaQuery();
+        GameServer server = gameServerService.getOne(wrapper.eq(GameServer::getId, gameStatOrder.getServerId()));
+        Date openTime = server.getOpenTime();
+        Date todayDate = DateUtils.todayDate();
+        int num = DateUtils.monthsBetween(openTime, todayDate);
         List<Date> monthsDateList = new ArrayList<>(num);
         for (int i = 0; i <= num; i++) {
-            monthsDateList.add(DateUtils.minusDays(gameStatOrder.getEndDate(), i));
+            monthsDateList.add(DateUtils.minusDays(todayDate, i));
         }
         return queryGameStatOrderList(gameStatOrder, monthsDateList, CommonConstant.PAY_ORDER_STAT_TYPE_MONTH);
     }
@@ -130,25 +142,25 @@ public class GameStatOrderServiceImpl extends ServiceImpl<GameStatOrderMapper, G
                 // 付款率
                 item.setPayRate(BigDecimalUtil.dividePercent(BigDecimalUtil.divide((double) item.getPayNum(), activeNum, 4).doubleValue()).toString());
                 // arpu
-                item.setArpu(BigDecimalUtil.divide(item.getAmount().doubleValue(), activeNum));
+                item.setArpu(BigDecimalUtil.divide(item.getAmount().doubleValue(), activeNum, 2));
             } else {
-                item.setPayRate("0.00");
-                item.setArpu(new BigDecimal(0));
+                item.setPayRate("0");
+                item.setArpu(BigDecimal.valueOf(0.00));
             }
 
             // arppu
             if (item.getPayNum() != null && item.getPayNum() != 0) {
-                item.setArppu(BigDecimalUtil.divide(item.getAmount().doubleValue(), item.getPayNum()));
+                item.setArppu(BigDecimalUtil.divide(item.getAmount().doubleValue(), item.getPayNum(), 2));
             } else {
-                item.setArppu(new BigDecimal(0));
+                item.setArppu(BigDecimal.valueOf(0.00));
             }
 
             // 服均充值
             if (item.getServerNum() != null && item.getServerNum() != 0) {
                 item.setServerAverageAmount(item.getAmount() != null ?
-                        BigDecimalUtil.divide(item.getAmount().doubleValue(), (double) item.getServerNum()) : new BigDecimal(0));
+                        BigDecimalUtil.divide(item.getAmount().doubleValue(), (double) item.getServerNum()) : BigDecimal.valueOf(0.00));
             } else {
-                item.setServerAverageAmount(new BigDecimal(0));
+                item.setServerAverageAmount(BigDecimal.valueOf(0.00));
             }
         });
 
