01e9f6cb1f8f01a2d9f1f7e4bd8c5863de4f624f
luopeihuan <luopeihuan@gzyouai.com>
2021-05-15 16:23:50 +0800

    去掉 jetcache 配置

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/controller/MilitaryStrengthListController.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/controller/MilitaryStrengthListController.java
@@ -1,7 +1,7 @@
 package org.jeecg.modules.game.controller;
 
 import cn.hutool.core.date.DatePattern;
-import cn.youai.xiuzhen.utils.ConvertUtils;
+import cn.youai.server.utils.ConvertUtils;
 import cn.youai.xiuzhen.utils.DateUtils;
 import com.alibaba.druid.util.StringUtils;
 import com.alibaba.excel.EasyExcel;
