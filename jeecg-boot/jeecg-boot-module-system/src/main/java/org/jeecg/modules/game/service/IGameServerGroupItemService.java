f4226ed48f953680a5d02a5c559c7ccca3bba74d
ocean <linzhengqiang@gzyouai.com>
2021-06-21 17:26:05 +0800

    节日活动-结义排行榜

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/IGameServerGroupItemService.java
@@ -0,0 +1,19 @@
+/*
+* create by mybatis-plus-generator  https://github.com/xiweile
+*/
+package org.jeecg.modules.game.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.jeecg.modules.game.entity.GameServerGroupItem;
+
+/**
+ * <p>
+ * 游戏渠道服配置 服务类
+ * </p>
+ *
+ * @author luopeihuan
+ * @since 2021-04-22
+ */
+public interface IGameServerGroupItemService extends IService<GameServerGroupItem> {
+
+}
