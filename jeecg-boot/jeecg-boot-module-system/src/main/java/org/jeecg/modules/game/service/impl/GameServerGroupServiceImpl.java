54b97a7f61a5426784da58108991b5d888f8a74d
luopeihuan <luopeihuan@gzyouai.com>
2021-08-03 17:17:15 +0800

    新增 GameServerGroup

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/impl/GameServerGroupServiceImpl.java
@@ -0,0 +1,23 @@
+/*
+ * create by mybatis-plus-generator  https://github.com/xiweile
+ */
+package org.jeecg.modules.game.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.jeecg.modules.game.entity.GameServerGroup;
+import org.jeecg.modules.game.mapper.GameServerGroupMapper;
+import org.jeecg.modules.game.service.IGameServerGroupService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 游戏渠道服配置 服务实现类
+ * </p>
+ *
+ * @author luopeihuan
+ * @since 2021-08-03
+ */
+@Service
+public class GameServerGroupServiceImpl extends ServiceImpl<GameServerGroupMapper, GameServerGroup> implements IGameServerGroupService {
+
+}
