a6e85bedb9cd164a2dce7d02b8679c57c4e8300c
luopeihuan <luopeihuan@gzyouai.com>
2021-12-07 22:07:57 +0800

    优化订单查询

---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/game/service/IGamePlayerService.java
@@ -0,0 +1,19 @@
+/*
+ * create by mybatis-plus-generator  https://github.com/xiweile
+ */
+package org.jeecg.modules.game.service;
+
+import cn.youai.entities.GamePlayer;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 玩家表 服务类
+ * </p>
+ *
+ * @author buliangliang
+ * @since 2021-12-07
+ */
+public interface IGamePlayerService extends IService<GamePlayer> {
+
+}
