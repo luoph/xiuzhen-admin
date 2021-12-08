163ff2640321f86f02500bebe3edcbed7f5d3c16
luopeihuan <luopeihuan@gzyouai.com>
2021-12-08 23:08:55 +0800

    优化玩家道具日志查询

--- jeecg-vue/src/views/player/PlayerItemLogList.vue
+++ jeecg-vue/src/views/player/PlayerItemLogList.vue
@@ -4,31 +4,31 @@
         <div class="table-page-search-wrapper">
             <a-form layout="inline" @keyup.enter.native="searchQuery">
                 <a-row :gutter="24">
-                    <a-col :md="5" :sm="8">
+                    <a-col :md="4" :sm="8">
                         <a-form-item label="玩家id">
                             <a-input placeholder="请输入玩家id" v-model="queryParam.playerId"></a-input>
                         </a-form-item>
                     </a-col>
-                    <a-col :md="5" :sm="8">
+                    <a-col :md="4" :sm="8">
                         <a-form-item label="道具id">
                             <a-input placeholder="请输入道具id" v-model="queryParam.itemId"></a-input>
                         </a-form-item>
                     </a-col>
-                    <a-col :md="9" :sm="6">
-                        <a-form-item label="统计日期">
-                            <a-range-picker format="YYYY-MM-DD" :placeholder="['开始日期', '结束日期']" @change="onDateChange" />
-                        </a-form-item>
-                    </a-col>
-                    <a-col :md="3" :sm="3">
+                    <a-col :md="4" :sm="8">
                         <a-form-item label="产销类型">
                             <a-select placeholder="产销类型" v-model="queryParam.type" @change="resetWay">
                                 <a-select-option value="">全部</a-select-option>
-                                <a-select-option value="1">产出</a-select-option>
+                                <a-select-option value="1">获得</a-select-option>
                                 <a-select-option value="2">消耗</a-select-option>
                             </a-select>
                         </a-form-item>
                     </a-col>
-                    <a-col :md="8" :sm="5">
+                    <a-col :md="8" :sm="16">
+                        <a-form-item label="统计日期">
+                            <a-range-picker v-model="queryParam.createTimeRange" format="YYYY-MM-DD" :placeholder="['开始时间', '结束时间']" @change="onDateChange" />
+                        </a-form-item>
+                    </a-col>
+                    <a-col :md="6" :sm="16">
                         <a-form-item v-if="queryParam.type === '1'" key="1" label="产出途径">
                             <a-select-read-json-some json-file="item_fall_rule" placeholder="请选择途径" @onSelectOptionSome="selectWay"></a-select-read-json-some>
                         </a-form-item>
@@ -39,6 +39,7 @@
                     <a-col :md="6" :sm="8">
                         <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
                             <a-button type="primary" icon="search" @click="searchQuery">查询</a-button>
+                            <a-button type="primary" icon="reload" style="margin-left: 8px" @click="searchReset">重置</a-button>
                         </span>
                     </a-col>
                 </a-row>
@@ -75,6 +76,7 @@ import PlayerItemLogModal from "./modules/PlayerItemLogModal";
 import ServerSelect from "@/components/gameserver/ServerSelect";
 import ASelectReadJsonSome from "@comp/gameserver/ASelectReadJsonSome";
 import JDate from "@/components/jeecg/JDate.vue";
+import { filterObj } from "@/utils/util";
 import moment from "moment";
 
 export default {
@@ -147,7 +149,7 @@ export default {
                     align: "center",
                     dataIndex: "type",
                     customRender: function(text) {
-                        return text === 1 ? "存入" : "支出";
+                        return text === 1 ? "获得" : "消耗";
                     }
                 },
                 {
@@ -170,12 +172,22 @@ export default {
     },
     methods: {
         initDictConfig() {},
+        getQueryParams() {
+            console.log(this.queryParam.createTimeRange);
+            var param = Object.assign({}, this.queryParam, this.isorter);
+            param.pageNo = this.ipagination.current;
+            param.pageSize = this.ipagination.pageSize;
+            // 范围参数不传递后台
+            delete param.createTimeRange;
+            return filterObj(param);
+        },
         selectServerId(serverId) {
             this.queryParam.serverId = serverId;
         },
         onDateChange(dates, dateStrings) {
-            this.queryParam.startDate = dateStrings[0];
-            this.queryParam.endDate = dateStrings[1];
+            console.log(dateStrings[0], dateStrings[1]);
+            this.queryParam.createTime_begin = dateStrings[0];
+            this.queryParam.createTime_end = dateStrings[1];
         },
         selectWay(way) {
             if (way.length > 0) {
