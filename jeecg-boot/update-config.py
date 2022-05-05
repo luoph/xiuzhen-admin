5de210d2f52a82609255b7a37753e40ee752c1f2
luopeihuan <luopeihuan@gzyouai.com>
2022-05-05 18:04:15 +0800

    优化配置同步脚本

--- jeecg-boot/update-config.py
+++ jeecg-boot/update-config.py
@@ -24,6 +24,12 @@ if PY2:
     sys.setdefaultencoding('utf-8')
 
 REMOTE_URL = u"{}/doc_xiuzhen/A-%E5%AF%BC%E8%A1%A8/json/{}/server"
+SEARCH_DIR = 'jeecg-boot-module-system'
+TARGET_FILE = 'GameConfig.java'
+JSON_DIR = 'jeecg-boot-module-system/src/main/resources/json'
+
+# 其他的json
+OTHER_JSON = ['task_module_type', 'item_fall_rule']
 
 # 匹配代码行
 CODE_MATCH = 'ConfigTable.valueOf'
@@ -69,7 +75,7 @@ def mkdir(dir):
         os.makedirs(dir)
 
 
-def copy_json(svn_url, source_folder, target_folder, config_list, other_config=None):
+def copy_json(svn_url, source_folder, target_folder, config_list, other_config=None, verbose=False):
     r = svn.remote.RemoteClient(svn_url)
     r.export(source_folder, revision=None, force=True)
 
@@ -86,8 +92,9 @@ def copy_json(svn_url, source_folder, target_folder, config_list, other_config=N
             for file_name in file_list:
                 if file_name.endswith('.json'):
                     base_name = os.path.splitext(file_name)[0]
-                    if (not check_file or base_name in config_list):
-                        logging.info(u'copy {}'.format(file_name))
+                    if not check_file or base_name in config_list:
+                        if verbose:
+                            logging.info(u'copy {}'.format(file_name))
                         shutil.copy2(os.path.join(path, file_name), target_folder)
 
     remove_dir(source_folder)
@@ -98,18 +105,22 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 
     parser = argparse.ArgumentParser(description='manual to this script')
-    parser.add_argument('--svn', type=str, default='svn://dev.1101stu.com:7690')
+    parser.add_argument('--svn', type=str, default='svn://49.235.114.251:7690')
     parser.add_argument('--profile', type=str, default='develop')
+    parser.add_argument('--verbose', dest='verbose', type=str, default='False')
+
     args = parser.parse_args()
 
     # 查找 RefMgr.ts
     config_file_path = ''
-    for root, dir_list, file_list in os.walk('jeecg-boot-module-system'):
-        for filename in fnmatch.filter(file_list, "GameConfig.java"):
+    for root, dirnames, filenames in os.walk(SEARCH_DIR):
+        for filename in fnmatch.filter(filenames, TARGET_FILE):
             config_file_path = os.path.join(root, filename)
+            logging.info('config_file_path:{}'.format(config_file_path))
             break
 
-    logging.info('config_file_path: {}'.format(config_file_path))
+    logging.info('start update-config, profile:{}'.format(args.profile))
     all_config = parse_config(config_file_path, CODE_LINE_REGEX, CODE_MATCH, CONFIG_QUOTE, CONFIG_QUOTE)
     repo_url = REMOTE_URL.format(args.svn, args.profile)
-    copy_json(repo_url, 'xiuzhen-config', 'jeecg-boot-module-system/src/main/resources/json', all_config)
+    copy_json(repo_url, 'xiuzhen-config', JSON_DIR, all_config, OTHER_JSON, args.verbose == 'True')
+    logging.info('finish update-config, profile:{}'.format(args.profile))
