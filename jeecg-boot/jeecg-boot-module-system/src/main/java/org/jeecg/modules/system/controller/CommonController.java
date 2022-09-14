2baa40e29b903848667fd83238089bc874e54d20
luopeihuan <luopeihuan@gzyouai.com>
2022-09-14 16:36:33 +0800

    删除重复代码

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/controller/CommonController.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/controller/CommonController.java
@@ -28,6 +28,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URLDecoder;
+
 /**
  * <p>
  * 用户表 前端控制器
@@ -47,20 +48,21 @@ public class CommonController {
     /**
      * 本地：local minio：minio 阿里：alioss
      */
-    @Value(value="${jeecg.uploadType}")
+    @Value(value = "${jeecg.uploadType}")
     private String uploadType;
 
     /**
-     * @Author 政辉
      * @return
+     * @Author 政辉
      */
     @GetMapping("/403")
-    public Result<?> noauth()  {
+    public Result<?> noauth() {
         return Result.error("没有权限，请联系管理员授权");
     }
 
     /**
      * 文件上传统一方法
+     *
      * @param request
      * @param response
      * @return
@@ -73,7 +75,7 @@ public class CommonController {
 
         //LOWCOD-2580 sys/common/upload接口存在任意文件上传漏洞
         if (oConvertUtils.isNotEmpty(bizPath)) {
-            if(bizPath.contains(SymbolConstant.SPOT_SINGLE_SLASH) || bizPath.contains(SymbolConstant.SPOT_DOUBLE_BACKSLASH)){
+            if (bizPath.contains(SymbolConstant.SPOT_SINGLE_SLASH) || bizPath.contains(SymbolConstant.SPOT_DOUBLE_BACKSLASH)) {
                 throw new JeecgBootException("上传目录bizPath，格式非法！");
             }
         }
@@ -81,41 +83,41 @@ public class CommonController {
         MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
         // 获取上传文件对象
         MultipartFile file = multipartRequest.getFile("file");
-        if(oConvertUtils.isEmpty(bizPath)){
-            if(CommonConstant.UPLOAD_TYPE_OSS.equals(uploadType)){
+        if (oConvertUtils.isEmpty(bizPath)) {
+            if (CommonConstant.UPLOAD_TYPE_OSS.equals(uploadType)) {
                 //未指定目录，则用阿里云默认目录 upload
                 bizPath = "upload";
                 //result.setMessage("使用阿里云文件上传时，必须添加目录！");
                 //result.setSuccess(false);
                 //return result;
-            }else{
+            } else {
                 bizPath = "";
             }
         }
-        if(CommonConstant.UPLOAD_TYPE_LOCAL.equals(uploadType)){
+        if (CommonConstant.UPLOAD_TYPE_LOCAL.equals(uploadType)) {
             //update-begin-author:lvdandan date:20200928 for:修改JEditor编辑器本地上传
-            savePath = this.uploadLocal(file,bizPath);
+            savePath = this.uploadLocal(file, bizPath);
             //update-begin-author:lvdandan date:20200928 for:修改JEditor编辑器本地上传
             /**  富文本编辑器及markdown本地上传时，采用返回链接方式
-            //针对jeditor编辑器如何使 lcaol模式，采用 base64格式存储
-            String jeditor = request.getParameter("jeditor");
-            if(oConvertUtils.isNotEmpty(jeditor)){
-                result.setMessage(CommonConstant.UPLOAD_TYPE_LOCAL);
-                result.setSuccess(true);
-                return result;
-            }else{
-                savePath = this.uploadLocal(file,bizPath);
-            }
-            */
-        }else{
+             //针对jeditor编辑器如何使 lcaol模式，采用 base64格式存储
+             String jeditor = request.getParameter("jeditor");
+             if(oConvertUtils.isNotEmpty(jeditor)){
+             result.setMessage(CommonConstant.UPLOAD_TYPE_LOCAL);
+             result.setSuccess(true);
+             return result;
+             }else{
+             savePath = this.uploadLocal(file,bizPath);
+             }
+             */
+        } else {
             //update-begin-author:taoyan date:20200814 for:文件上传改造
             savePath = CommonUtils.upload(file, bizPath, uploadType);
             //update-end-author:taoyan date:20200814 for:文件上传改造
         }
-        if(oConvertUtils.isNotEmpty(savePath)){
+        if (oConvertUtils.isNotEmpty(savePath)) {
             result.setMessage(savePath);
             result.setSuccess(true);
-        }else {
+        } else {
             result.setMessage("上传失败！");
             result.setSuccess(false);
         }
@@ -124,15 +126,16 @@ public class CommonController {
 
     /**
      * 本地文件上传
-     * @param mf 文件
-     * @param bizPath  自定义路径
+     *
+     * @param mf      文件
+     * @param bizPath 自定义路径
      * @return
      */
-    private String uploadLocal(MultipartFile mf,String bizPath){
+    private String uploadLocal(MultipartFile mf, String bizPath) {
         try {
             String ctxPath = uploadpath;
             String fileName = null;
-            File file = new File(ctxPath + File.separator + bizPath + File.separator );
+            File file = new File(ctxPath + File.separator + bizPath + File.separator);
             if (!file.exists()) {
                 // 创建文件根目录
                 file.mkdirs();
@@ -140,18 +143,18 @@ public class CommonController {
             // 获取文件名
             String orgName = mf.getOriginalFilename();
             orgName = CommonUtils.getFileName(orgName);
-            if(orgName.indexOf(SymbolConstant.SPOT)!=-1){
+            if (orgName.indexOf(SymbolConstant.SPOT) != -1) {
                 fileName = orgName.substring(0, orgName.lastIndexOf(".")) + "_" + System.currentTimeMillis() + orgName.substring(orgName.lastIndexOf("."));
-            }else{
-                fileName = orgName+ "_" + System.currentTimeMillis();
+            } else {
+                fileName = orgName + "_" + System.currentTimeMillis();
             }
             String savePath = file.getPath() + File.separator + fileName;
             File savefile = new File(savePath);
             FileCopyUtils.copy(mf.getBytes(), savefile);
             String dbpath = null;
-            if(oConvertUtils.isNotEmpty(bizPath)){
+            if (oConvertUtils.isNotEmpty(bizPath)) {
                 dbpath = bizPath + File.separator + fileName;
-            }else{
+            } else {
                 dbpath = fileName;
             }
             if (dbpath.contains(SymbolConstant.DOUBLE_BACKSLASH)) {
@@ -212,34 +215,35 @@ public class CommonController {
     public void view(HttpServletRequest request, HttpServletResponse response) {
         // ISO-8859-1 ==> UTF-8 进行编码转换
         String imgPath = extractPathFromPattern(request);
-        if(oConvertUtils.isEmpty(imgPath) || CommonConstant.STRING_NULL.equals(imgPath)){
+        if (oConvertUtils.isEmpty(imgPath) || CommonConstant.STRING_NULL.equals(imgPath)) {
             return;
         }
         // 其余处理略
         InputStream inputStream = null;
         OutputStream outputStream = null;
         try {
-            imgPath = imgPath.replace("..", "").replace("../","");
+            imgPath = imgPath.replace("..", "").replace("../", "");
             if (imgPath.endsWith(SymbolConstant.COMMA)) {
                 imgPath = imgPath.substring(0, imgPath.length() - 1);
             }
             String filePath = uploadpath + File.separator + imgPath;
             File file = new File(filePath);
-            if(!file.exists()){
+            if (!file.exists()) {
                 response.setStatus(404);
-                throw new RuntimeException("文件["+imgPath+"]不存在..");
+//                throw new RuntimeException("文件["+imgPath+"]不存在..");
+            } else {
+                // 设置强制下载不打开
+                response.setContentType("application/force-download");
+                response.addHeader("Content-Disposition", "attachment;fileName=" + new String(file.getName().getBytes("UTF-8"), "iso-8859-1"));
+                inputStream = new BufferedInputStream(new FileInputStream(filePath));
+                outputStream = response.getOutputStream();
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = inputStream.read(buf)) > 0) {
+                    outputStream.write(buf, 0, len);
+                }
+                response.flushBuffer();
             }
-            // 设置强制下载不打开
-            response.setContentType("application/force-download");
-            response.addHeader("Content-Disposition", "attachment;fileName=" + new String(file.getName().getBytes("UTF-8"),"iso-8859-1"));
-            inputStream = new BufferedInputStream(new FileInputStream(filePath));
-            outputStream = response.getOutputStream();
-            byte[] buf = new byte[1024];
-            int len;
-            while ((len = inputStream.read(buf)) > 0) {
-                outputStream.write(buf, 0, len);
-            }
-            response.flushBuffer();
         } catch (IOException e) {
             log.error("预览文件失败" + e.getMessage());
             response.setStatus(404);
@@ -322,9 +326,9 @@ public class CommonController {
 //	}
 
     /**
-     * @功能：pdf预览Iframe
      * @param modelAndView
      * @return
+     * @功能：pdf预览Iframe
      */
     @RequestMapping("/pdf/pdfPreviewIframe")
     public ModelAndView pdfPreviewIframe(ModelAndView modelAndView) {
@@ -333,8 +337,9 @@ public class CommonController {
     }
 
     /**
-     *  把指定URL后的字符串全部截断当成参数
-     *  这么做是为了防止URL中包含中文或者特殊字符（/等）时，匹配不了的问题
+     * 把指定URL后的字符串全部截断当成参数
+     * 这么做是为了防止URL中包含中文或者特殊字符（/等）时，匹配不了的问题
+     *
      * @param request
      * @return
      */
@@ -371,7 +376,7 @@ public class CommonController {
             headers.set("X-Access-Token", token);
             // 发送请求
             String httpUrl = URLDecoder.decode(url, "UTF-8");
-            ResponseEntity<String> response = RestUtil.request(httpUrl, method, headers , variables, params, String.class);
+            ResponseEntity<String> response = RestUtil.request(httpUrl, method, headers, variables, params, String.class);
             // 封装返回结果
             Result<Object> result = new Result<>();
             int statusCode = response.getStatusCodeValue();
