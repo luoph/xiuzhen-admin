52ecc84f6d6cbd1fc021b1ab6ce11898082600c9
luopeihuan <luopeihuan@gzyouai.com>
2022-12-12 22:59:39 +0800

    修复在线表单无法导入数据库表问题

--- jeecg-vue/vue.config.js
+++ jeecg-vue/vue.config.js
@@ -118,7 +118,7 @@ module.exports = {
       /* 注意：jeecgboot前端做了改造，此处不需要配置跨域和后台接口（只需要改.env相关配置文件即可）
           issues/3462 很多人此处做了配置，导致刷新前端404问题，请一定注意*/
       '/jeecg-boot': {
-        target: process.env.VUE_APP_API_BASE_URL,
+        target: 'http://localhost:8080',
         ws: false,
         changeOrigin: true
       },
