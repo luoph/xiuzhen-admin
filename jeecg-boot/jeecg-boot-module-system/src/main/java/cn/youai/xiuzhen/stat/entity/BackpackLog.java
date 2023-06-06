37768258c34f9c1d99a7813d782d4ab1588ae7c5
ocean <linzhengqiang@gzyouai.com>
2023-06-06 15:18:16 +0800

    修复道具日志导出

--- jeecg-boot/jeecg-boot-module-system/src/main/java/cn/youai/xiuzhen/stat/entity/BackpackLog.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/cn/youai/xiuzhen/stat/entity/BackpackLog.java
@@ -1,10 +1,13 @@
 package cn.youai.xiuzhen.stat.entity;
 
 import cn.youai.xiuzhen.core.base.IPlayerData;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-import lombok.experimental.Accessors;
+import org.jeecg.common.constant.TimeConstant;
+import org.jeecgframework.poi.excel.annotation.Excel;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -16,7 +19,6 @@ import java.util.Date;
  * @since 202109131547
  */
 @Data
-@Accessors(chain = true)
 @Document(collection = "backpack_log")
 public class BackpackLog implements IPlayerData, Serializable {
 
@@ -26,44 +28,64 @@ public class BackpackLog implements IPlayerData, Serializable {
      * 自增id
      */
     @Id
+    @Excel(name = "id", width = 15)
     private String _id;
 
+    @Excel(name = "玩家id", width = 15)
     private Long playerId;
 
+    @Excel(name = "昵称", width = 15)
     private String nickname;
 
+    @Excel(name = "区服id", width = 15)
     private Integer serverId;
 
+    @Excel(name = "创角区服", width = 15)
     private Integer sid;
 
+    @Excel(name = "道具id", width = 15)
     private Integer itemId;
 
+    @Excel(name = "道具名", width = 15)
     private String itemName;
 
+    @Excel(name = "数量", width = 15)
     private Long num;
 
     /**
      * 1存入 2消耗
      */
+    @Excel(name = "方式", width = 15)
     private Integer type;
 
     /**
      * 获取途径
      */
+    @Excel(name = "途径", width = 15)
     private Integer way;
 
     /**
      * 途径多选
      */
+    @Excel(name = "途径多选", width = 15)
     private String ways;
 
+    @Excel(name = "途径名", width = 15)
     private String wayName;
 
+    @Excel(name = "更新前数量", width = 15)
     private Long beforeNum;
 
+    @Excel(name = "更新后数量", width = 15)
     private Long afterNum;
 
+    @Excel(name = "创建日期", width = 15, format = TimeConstant.DEFAULT_DATE_FORMAT)
+    @JsonFormat(timezone = TimeConstant.DEFAULT_TIMEZONE, pattern = TimeConstant.DEFAULT_DATE_FORMAT)
+    @DateTimeFormat(pattern = TimeConstant.DEFAULT_DATE_FORMAT)
     private Date createDate;
 
+    @Excel(name = "创建时间", width = 20, format = TimeConstant.DEFAULT_TIME_FORMAT)
+    @JsonFormat(timezone = TimeConstant.DEFAULT_TIMEZONE, pattern = TimeConstant.DEFAULT_TIME_FORMAT)
+    @DateTimeFormat(pattern = TimeConstant.DEFAULT_TIME_FORMAT)
     private Date createTime;
 }
